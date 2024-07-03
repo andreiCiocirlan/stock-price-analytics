e077cc2c343df25a32297094373324e71c6919e0
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-07-03 18:27:50 +0300

    remove old code

--- src/main/java/stock/price/analytics/repository/StockWeeklyHistoricalPricesRepository.java
+++
@@ -1,9 +0,0 @@
-package stock.price.analytics.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import stock.price.analytics.model.prices.StockWeeklyHistoricalPrices;
-
-@Repository
-public interface StockWeeklyHistoricalPricesRepository extends JpaRepository<StockWeeklyHistoricalPrices, Long> {
-}
