101f1e77009f968bdb92dbb34961c085e8bba2ee
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-07-06 09:51:52 +0300

    refresh MV

---
+++ src/main/java/stock/price/analytics/repository/RefreshMaterializedViewsRepository.java
@@ -0,0 +1,27 @@
+package stock.price.analytics.repository;
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import stock.price.analytics.model.prices.ohlc.AbstractPriceOHLC;
+
+@Repository
+public interface RefreshMaterializedViewsRepository extends JpaRepository<AbstractPriceOHLC, Long> {
+
+    @Modifying
+    @Transactional
+    @Query(value = "REFRESH MATERIALIZED VIEW weekly_prices_view", nativeQuery = true)
+    void refreshWeeklyPrices();
+
+    @Modifying
+    @Transactional
+    @Query(value = "REFRESH MATERIALIZED VIEW monthly_prices_view", nativeQuery = true)
+    void refreshMonthlyPrices();
+
+    @Modifying
+    @Transactional
+    @Query(value = "REFRESH MATERIALIZED VIEW yearly_prices_view", nativeQuery = true)
+    void refreshYearlyPrices();
+}
