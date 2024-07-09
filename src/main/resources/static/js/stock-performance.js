de3b71a793c3a22c932313dcab2b9c171e20a246
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-07-09 17:24:59 +0300

    cleanup UI

--- src/main/resources/static/js/stock-performance.js
+++ src/main/resources/static/js/stock-performance.js
@@ -70,22 +70,40 @@ function updateStockPerformanceChartWithData(data, timeFrame, numRows, numCols, 
     stockPerformanceChart = Highcharts.chart('container', {
         chart: { type: 'heatmap', backgroundColor: '#171B26' },
         tooltip: { enabled: false },
-        title: { text: `${timeFrame}` + ' Heatmap', style: { color: '#FFFFFF' } },
+        title: null,
         xAxis: { visible: false, categories: Array.from({ length: numCols }, (_, i) => `Col ${i + 1}`) },
         yAxis: { visible: false, categories: Array.from({ length: numRows }, (_, i) => `Row ${i + 1}`) },
-        legend: { itemStyle: { color: '#FFFFFF' } },
+        legend: {
+            align: 'center',
+            layout: 'horizontal',
+            symbolType: 'square',
+            labelFormatter: function() {
+                let legendItem = `${this.to.toFixed(1)}`;
+                if (legendItem != 0.5) {
+                    if (legendItem > 0) {
+                        legendItem = `${this.from.toFixed(1)}`;
+                    }
+                } else {
+                    legendItem = 0;
+                }
+                return `<div class="legend-item-container">
+                             <div class="legend-item" style="background-color:${this.color};color:white;font-weight:bold;">${legendItem}%</div>
+                          </div>`;
+            },
+            useHTML: true
+        },
         colorAxis: {
             dataClasses: [
                 { from: -100, to: -10.3, color: '#E53935' }, // Bright red for performance < -8.3
                 { from: -10.3, to: -5.6, color: '#C82925' },
                 { from: -5.6, to: -2.9, color: '#AF3835' },
                 { from: -2.9, to: -0.5, color: '#7D2A27' },
-                { from: -0.5, to: 0, color: '#494949' }, // Gray for performance between -0.5 and 0
-                { from: 0, to: 0.5, color: '#494949' },
+                { from: -0.5, to: 0.5, color: '#494949' }, // Gray for performance between -0.5 and 0
                 { from: 0.5, to: 2.9, color: '#165E45' },
                 { from: 2.9, to: 5.6, color: '#027B52' },
                 { from: 5.6, to: 10.3, color: '#00A06A' },
-                { from: 10.3, to: 10000, color: '#00B24D' } // Very green for performance > 8.3
+                { from: 10.3, to: 100, color: '#00B24D' },
+                { from: 100, to: 100000, color: '#00B24D' } // Very green for performance > 10.3
             ]
         },
         plotOptions: {
