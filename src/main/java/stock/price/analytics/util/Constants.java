e765b383ae3c8bc124285e3cb3ad94396fcf9c2d
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-07-11 23:04:59 +0300

    revive finnhub client

--- src/main/java/stock/price/analytics/util/Constants.java
+++ src/main/java/stock/price/analytics/util/Constants.java
@@ -3,7 +3,7 @@ package stock.price.analytics.util;
 public class Constants {
 
     public static final String STOCKS_LOCATION = "C:\\Users\\andre\\Downloads\\stocks-data\\historical_prices\\ALL_STOCKS\\";
-    public static final String BASE_URL = "https://finnhub.io/api/v1";
+    public static final String FINNHUB_BASE_URL = "https://finnhub.io/api/v1";
     public static final int NR_THREADS = 10;
     public static final String CLASSPATH_TICKERS_XTB_TXT = "classpath:tickers_XTB.txt";
 }
