2598a08244b3f542d6f49ea56eaff9ff703ab253
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-07-13 18:27:43 +0300

    unused for now

--- src/main/java/stock/price/analytics/model/quote/QuoteData.java
+++
@@ -1,9 +0,0 @@
-package stock.price.analytics.model.quote;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class QuoteData{
-
-	@JsonProperty("quoteResponse")
-	private QuoteResponse quoteResponse;
-}
