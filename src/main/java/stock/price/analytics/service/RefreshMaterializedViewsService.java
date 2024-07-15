f05a569e56071ef99072edacf07db462d76fd167
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-07-15 16:11:13 +0300

    add logging

--- src/main/java/stock/price/analytics/service/RefreshMaterializedViewsService.java
+++ src/main/java/stock/price/analytics/service/RefreshMaterializedViewsService.java
@@ -14,12 +14,19 @@ public class RefreshMaterializedViewsService {
 
     public void refreshMaterializedViews() {
         refreshMaterializedViewsRepository.refreshLatestPrices();
+        log.info("refreshed latest prices");
         refreshMaterializedViewsRepository.refreshDailyPerformanceHeatmapPrices();
+        log.info("refreshed heatmap prices");
         refreshMaterializedViewsRepository.refreshWeeklyPerformanceHeatmapPrices();
+        log.info("refreshed weekly performance prices");
         refreshMaterializedViewsRepository.refreshMonthlyPerformanceHeatmapPrices();
+        log.info("refreshed monthly performance prices");
         refreshMaterializedViewsRepository.refreshYearlyPerformanceHeatmapPrices();
+        log.info("refreshed yearly performance prices");
         refreshMaterializedViewsRepository.refreshHighLow4w();
+        log.info("refreshed high low 4w");
         refreshMaterializedViewsRepository.refreshHighLow52w();
+        log.info("refreshed high low 52w");
     }
 
 }
