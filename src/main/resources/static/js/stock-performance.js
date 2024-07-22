878e47a651c8f1ef32e372f38ace16d9b1286ca4
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-07-22 09:50:51 +0300

    mobile remove openStockGraph

--- src/main/resources/static/js/stock-performance.js
+++ src/main/resources/static/js/stock-performance.js
@@ -119,13 +119,6 @@ function updateStockPerformanceChartWithData(data, timeFrame, numRows, numCols, 
                     return `<span style="font-size: ${fontSize};">${data[this.point.index].ticker}<br>${data[this.point.index].performance}%</span>`;
                 }
             },
-            point: {
-                events: {
-                    click: function() {
-                        openStockGraph({ ticker: data[this.index].ticker });
-                    }
-                }
-            },
             data:
                 data.map((item, index) => [
                         Math.floor(index / numRows),
