3283cdd5c7946ab2d22a72aaf1f6709ceeade869
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-08-11 21:26:42 +0300

    update ipo_date, delisted_date for stocks at app start-up

--- src/main/java/stock/price/analytics/Application.java
+++ src/main/java/stock/price/analytics/Application.java
@@ -1,15 +1,27 @@
 package stock.price.analytics;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import stock.price.analytics.repository.stocks.StockRepository;
 
 @Slf4j
+@RequiredArgsConstructor
 @SpringBootApplication
-public class Application {
+public class Application implements ApplicationRunner {
+
+    private final StockRepository stockRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // update ipo/delisted dates at start-up
+        stockRepository.updateIpoAndDelistedDates();
+    }
 }
--- src/main/java/stock/price/analytics/repository/stocks/StockRepository.java
+++ src/main/java/stock/price/analytics/repository/stocks/StockRepository.java
@@ -1,9 +1,25 @@
 package stock.price.analytics.repository.stocks;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import stock.price.analytics.model.stocks.Stock;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
+
+    @Query(value = """
+            UPDATE stocks s
+            SET delisted_date = (
+              SELECT MAX(dp.date)
+              FROM daily_prices dp
+              WHERE dp.ticker = s.ticker
+              HAVING MAX(dp.date) < CURRENT_DATE - INTERVAL '20 day'
+            ), ipo_date = (
+              SELECT MIN(dp.date)
+              FROM daily_prices dp
+              WHERE dp.ticker = s.ticker
+            )
+            """, nativeQuery = true)
+    void updateIpoAndDelistedDates();
 }
