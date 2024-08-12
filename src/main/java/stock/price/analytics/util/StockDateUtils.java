601ad0089c961769cc2af2c0cd4d041860ef47f0
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-08-12 17:59:16 +0300

    update logic for same week

--- src/main/java/stock/price/analytics/util/StockDateUtils.java
+++ src/main/java/stock/price/analytics/util/StockDateUtils.java
@@ -1,12 +1,19 @@
 package stock.price.analytics.util;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 
 public class StockDateUtils {
 
-    public static boolean sameWeek(LocalDate date, LocalDate startOfWeek) {
-        return date.isAfter(startOfWeek.minusDays(7)) && date.isBefore(startOfWeek.plusDays(7));
-    }
+    public static boolean sameWeek(LocalDate date, LocalDate endDate) {
+        // Find the start of the week (Monday)
+        LocalDate weekStart = endDate.with(DayOfWeek.MONDAY);
+
+        // Calculate the end of the week (Friday)
+        LocalDate weekEnd = weekStart.plusDays(4); // 4 days after Monday is Friday
+
+        // Check if the date is within the range of the week
+        return !date.isBefore(weekStart) && !date.isAfter(weekEnd);    }
 
     public static boolean sameMonth(LocalDate date, LocalDate startOfMonth) {
         return date.getYear() == startOfMonth.getYear() && date.getMonthValue() == startOfMonth.getMonthValue();
