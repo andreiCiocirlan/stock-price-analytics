13623d9a390267572a954e9477470b31405f99bd
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-08-23 15:16:31 +0300

    refactor

--- src/main/java/stock/price/analytics/service/RefreshMaterializedViewsService.java
+++ src/main/java/stock/price/analytics/service/RefreshMaterializedViewsService.java
@@ -12,30 +12,48 @@ public class RefreshMaterializedViewsService {
 
     private final RefreshMaterializedViewsRepository refreshMaterializedViewsRepository;
 
+    public void refreshAllMaterializedViews() {
+        refreshHighLowPrices();
+        refreshLatestAndPerformanceHeatmapPrices();
+        refreshPreviousHighTimeframePrices();
+    }
+
     public void refreshMaterializedViews(boolean refreshHighLowPrices) {
         if (refreshHighLowPrices) {
-            refreshMaterializedViewsRepository.refreshHighLow4w();
-            log.info("refreshed high low 4w");
-            refreshMaterializedViewsRepository.refreshHighLow52w();
-            log.info("refreshed high low 52w");
+            refreshHighLowPrices();
         } else {
-            refreshMaterializedViewsRepository.refreshLatestPrices();
-            log.info("refreshed latest prices");
-            refreshMaterializedViewsRepository.refreshDailyPerformanceHeatmapPrices();
-            log.info("refreshed daily performance prices");
-            refreshMaterializedViewsRepository.refreshWeeklyPerformanceHeatmapPrices();
-            log.info("refreshed weekly performance prices");
-            refreshMaterializedViewsRepository.refreshMonthlyPerformanceHeatmapPrices();
-            log.info("refreshed monthly performance prices");
-            refreshMaterializedViewsRepository.refreshYearlyPerformanceHeatmapPrices();
-            log.info("refreshed yearly performance prices");
-            refreshMaterializedViewsRepository.refreshPrevTwoWeeks();
-            log.info("refreshed prev two weeks prices");
-            refreshMaterializedViewsRepository.refreshPrevTwoMonths();
-            log.info("refreshed prev two months prices");
-            refreshMaterializedViewsRepository.refreshPrevTwoYears();
-            log.info("refreshed prev two years prices");
+            refreshLatestAndPerformanceHeatmapPrices();
+            refreshPreviousHighTimeframePrices();
         }
+    }
+
+    private void refreshHighLowPrices() {
+        refreshMaterializedViewsRepository.refreshHighLow4w();
+        log.info("refreshed high low 4w");
+        refreshMaterializedViewsRepository.refreshHighLow52w();
+        log.info("refreshed high low 52w");
+    }
+
+    private void refreshLatestAndPerformanceHeatmapPrices() {
+        refreshMaterializedViewsRepository.refreshLatestPrices();
+        log.info("refreshed latest prices");
+        refreshMaterializedViewsRepository.refreshDailyPerformanceHeatmapPrices();
+        log.info("refreshed daily performance prices");
+        refreshMaterializedViewsRepository.refreshWeeklyPerformanceHeatmapPrices();
+        log.info("refreshed weekly performance prices");
+        refreshMaterializedViewsRepository.refreshMonthlyPerformanceHeatmapPrices();
+        log.info("refreshed monthly performance prices");
+        refreshMaterializedViewsRepository.refreshYearlyPerformanceHeatmapPrices();
+        log.info("refreshed yearly performance prices");
+    }
+
+    private void refreshPreviousHighTimeframePrices() {
+        refreshMaterializedViewsRepository.refreshPrevTwoWeeks();
+        log.info("refreshed prev two weeks prices");
+        refreshMaterializedViewsRepository.refreshPrevTwoMonths();
+        log.info("refreshed prev two months prices");
+        refreshMaterializedViewsRepository.refreshPrevTwoYears();
+        log.info("refreshed prev two years prices");
     }
 
 }
