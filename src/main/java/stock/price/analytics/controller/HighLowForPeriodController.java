7d2551fca3fcc1c721e87de70f79c3443c6920ed
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-09-15 21:51:44 +0300

    save hl 4w 52w for ticker and tradingDate

--- src/main/java/stock/price/analytics/controller/HighLowForPeriodController.java
+++ src/main/java/stock/price/analytics/controller/HighLowForPeriodController.java
@@ -1,14 +1,17 @@
 package stock.price.analytics.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import stock.price.analytics.model.prices.enums.StockPerformanceInterval;
 import stock.price.analytics.service.HighLowForPeriodService;
 
-import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,14 +21,22 @@ public class HighLowForPeriodController {
 
     @PostMapping("/save-52w-high-low")
     @ResponseStatus(HttpStatus.OK)
-    public void save52WeekHighLowPrices() throws IOException {
+    public void save52WeekHighLowPrices() {
         highLowForPeriodService.saveHighLowPricesForPeriod(StockPerformanceInterval.STOCK_PERF_INTERVAL_52W);
     }
 
     @PostMapping("/save-30d-high-low")
     @ResponseStatus(HttpStatus.OK)
-    public void save30DayHighLowPrices() throws IOException {
+    public void save30DayHighLowPrices() {
         highLowForPeriodService.saveHighLowPricesForPeriod(StockPerformanceInterval.STOCK_PERF_INTERVAL_30D);
+    }
+
+    @PostMapping("/save-hl-4w-52w-ticker")
+    @ResponseStatus(HttpStatus.OK)
+    public void saveHighLow_4w_52wForTickerAndDate(@RequestParam("ticker") String ticker,
+                                                       @RequestParam(name = "tradingDate")
+                                                            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate tradingDate) {
+        highLowForPeriodService.saveOrUpdateHighLow_4w_52w(List.of(ticker), tradingDate);
     }
 
 }
