e44710b0a91fac110efb250824e961beb0955143
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-09-19 23:57:33 +0300

    add logTime util

---
+++ src/main/java/stock/price/analytics/util/LoggingUtil.java
@@ -0,0 +1,15 @@
+package stock.price.analytics.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LoggingUtil {
+
+    public static void logTime(Runnable func, String actionName) {
+        long start = System.nanoTime();
+        func.run();
+        long duration = (System.nanoTime() - start) / 1_000_000;
+        log.info("{} in {} ms", actionName, duration);
+    }
+
+}
