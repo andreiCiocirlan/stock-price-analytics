c706be5830afc74a312a1ff7ed133d16d668b9f0
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-09-20 00:01:09 +0300

    logTime usage

--- src/main/java/stock/price/analytics/service/RefreshMaterializedViewsService.java
+++ src/main/java/stock/price/analytics/service/RefreshMaterializedViewsService.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import stock.price.analytics.repository.materializedviews.RefreshMaterializedViewsRepository;
 
+import static stock.price.analytics.util.LoggingUtil.logTime;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -18,25 +20,17 @@ public class RefreshMaterializedViewsService {
     }
 
     private void refreshLatestAndPerformanceHeatmapPrices() {
-        refreshMaterializedViewsRepository.refreshLatestPrices();
-        log.info("refreshed latest prices");
-        refreshMaterializedViewsRepository.refreshDailyPerformanceHeatmapPrices();
-        log.info("refreshed daily performance prices");
-        refreshMaterializedViewsRepository.refreshWeeklyPerformanceHeatmapPrices();
-        log.info("refreshed weekly performance prices");
-        refreshMaterializedViewsRepository.refreshMonthlyPerformanceHeatmapPrices();
-        log.info("refreshed monthly performance prices");
-        refreshMaterializedViewsRepository.refreshYearlyPerformanceHeatmapPrices();
-        log.info("refreshed yearly performance prices");
+        logTime(refreshMaterializedViewsRepository::refreshLatestPrices, "refreshed latest prices");
+        logTime(refreshMaterializedViewsRepository::refreshDailyPerformanceHeatmapPrices, "refreshed daily performance prices");
+        logTime(refreshMaterializedViewsRepository::refreshWeeklyPerformanceHeatmapPrices, "refreshed weekly performance prices");
+        logTime(refreshMaterializedViewsRepository::refreshMonthlyPerformanceHeatmapPrices, "refreshed monthly performance prices");
+        logTime(refreshMaterializedViewsRepository::refreshYearlyPerformanceHeatmapPrices, "refreshed yearly performance prices");
     }
 
     private void refreshPreviousHighTimeframePrices() {
-        refreshMaterializedViewsRepository.refreshPrevTwoWeeks();
-        log.info("refreshed prev two weeks prices");
-        refreshMaterializedViewsRepository.refreshPrevTwoMonths();
-        log.info("refreshed prev two months prices");
-        refreshMaterializedViewsRepository.refreshPrevTwoYears();
-        log.info("refreshed prev two years prices");
+        logTime(refreshMaterializedViewsRepository::refreshPrevTwoWeeks, "refreshed prev two weeks prices");
+        logTime(refreshMaterializedViewsRepository::refreshPrevTwoMonths, "refreshed prev two months prices");
+        logTime(refreshMaterializedViewsRepository::refreshPrevTwoYears, "refreshed prev two years prices");
     }
 
 }
