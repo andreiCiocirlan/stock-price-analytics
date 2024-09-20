bf81b37d42548c878a458fe595b360d8cd8384a3
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2024-09-20 14:57:01 +0300

    refactor name

--- src/main/java/stock/price/analytics/service/RefreshMaterializedViewsService.java
+++ src/main/java/stock/price/analytics/service/RefreshMaterializedViewsService.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import stock.price.analytics.repository.materializedviews.RefreshMaterializedViewsRepository;
 
-import static stock.price.analytics.util.LoggingUtil.logTime;
+import static stock.price.analytics.util.LoggingUtil.logElapsedTime;
 
 @Slf4j
 @Service
@@ -20,17 +20,17 @@ public class RefreshMaterializedViewsService {
     }
 
     private void refreshLatestAndPerformanceHeatmapPrices() {
-        logTime(refreshMaterializedViewsRepository::refreshLatestPrices, "refreshed latest prices");
-        logTime(refreshMaterializedViewsRepository::refreshDailyPerformanceHeatmapPrices, "refreshed daily performance prices");
-        logTime(refreshMaterializedViewsRepository::refreshWeeklyPerformanceHeatmapPrices, "refreshed weekly performance prices");
-        logTime(refreshMaterializedViewsRepository::refreshMonthlyPerformanceHeatmapPrices, "refreshed monthly performance prices");
-        logTime(refreshMaterializedViewsRepository::refreshYearlyPerformanceHeatmapPrices, "refreshed yearly performance prices");
+        logElapsedTime(refreshMaterializedViewsRepository::refreshLatestPrices, "refreshed latest prices");
+        logElapsedTime(refreshMaterializedViewsRepository::refreshDailyPerformanceHeatmapPrices, "refreshed daily performance prices");
+        logElapsedTime(refreshMaterializedViewsRepository::refreshWeeklyPerformanceHeatmapPrices, "refreshed weekly performance prices");
+        logElapsedTime(refreshMaterializedViewsRepository::refreshMonthlyPerformanceHeatmapPrices, "refreshed monthly performance prices");
+        logElapsedTime(refreshMaterializedViewsRepository::refreshYearlyPerformanceHeatmapPrices, "refreshed yearly performance prices");
     }
 
     private void refreshPreviousHighTimeframePrices() {
-        logTime(refreshMaterializedViewsRepository::refreshPrevTwoWeeks, "refreshed prev two weeks prices");
-        logTime(refreshMaterializedViewsRepository::refreshPrevTwoMonths, "refreshed prev two months prices");
-        logTime(refreshMaterializedViewsRepository::refreshPrevTwoYears, "refreshed prev two years prices");
+        logElapsedTime(refreshMaterializedViewsRepository::refreshPrevTwoWeeks, "refreshed prev two weeks prices");
+        logElapsedTime(refreshMaterializedViewsRepository::refreshPrevTwoMonths, "refreshed prev two months prices");
+        logElapsedTime(refreshMaterializedViewsRepository::refreshPrevTwoYears, "refreshed prev two years prices");
     }
 
 }
--- src/main/java/stock/price/analytics/util/LoggingUtil.java
+++ src/main/java/stock/price/analytics/util/LoggingUtil.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LoggingUtil {
 
-    public static void logTime(Runnable func, String actionName) {
+    public static void logElapsedTime(Runnable func, String actionName) {
         long start = System.nanoTime();
         func.run();
         long duration = (System.nanoTime() - start) / 1_000_000;
