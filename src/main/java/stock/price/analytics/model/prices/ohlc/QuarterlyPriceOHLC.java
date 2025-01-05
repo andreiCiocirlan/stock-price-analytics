d692a7e666d2fbb918c69c9f2c5e184a9ec855f1
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-01-05 22:58:54 +0200

    quarterly prices support

---
+++ src/main/java/stock/price/analytics/model/prices/ohlc/QuarterlyPriceOHLC.java
@@ -0,0 +1,56 @@
+package stock.price.analytics.model.prices.ohlc;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+@Entity
+@Table(name = "quarterly_prices")
+@Getter
+@Setter
+@NoArgsConstructor
+public class QuarterlyPriceOHLC extends AbstractPriceOHLC {
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    public QuarterlyPriceOHLC(String ticker, LocalDate startDate, LocalDate endDate, CandleOHLC candleOHLC) {
+        super(ticker, candleOHLC);
+        this.startDate = startDate.with(TemporalAdjusters.firstDayOfMonth());
+        this.endDate = endDate.with(TemporalAdjusters.lastDayOfMonth());
+    }
+
+    public QuarterlyPriceOHLC(String ticker, LocalDate startDate, LocalDate endDate, double performance, CandleOHLC candleOHLC) {
+        super(ticker, candleOHLC);
+        this.startDate = startDate.with(TemporalAdjusters.firstDayOfYear());
+        this.endDate = endDate;
+        this.setPerformance(performance);
+    }
+
+    public static QuarterlyPriceOHLC newFrom(DailyPriceOHLC dailyPrices, double previousClose) {
+        return new QuarterlyPriceOHLC(
+                dailyPrices.getTicker(),
+                dailyPrices.getDate(),
+                dailyPrices.getDate(),
+                performanceFrom(dailyPrices, previousClose),
+                new CandleOHLC(dailyPrices.getOpen(), dailyPrices.getHigh(), dailyPrices.getLow(), dailyPrices.getClose()));
+    }
+
+    @Override
+    public String toString() {
+        return STR."Quarterly_OHLC {  StartDate=\{startDate} EndDate=\{endDate} \{super.toString()}";
+    }
+
+}
