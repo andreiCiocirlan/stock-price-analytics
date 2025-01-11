abb1cf8ced9ca3217c1cddae49d10142ef2acc09
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-01-11 19:33:55 +0200

    partitionDataAndSave

--- src/main/java/stock/price/analytics/service/PriceOHLCService.java
+++ src/main/java/stock/price/analytics/service/PriceOHLCService.java
@@ -20,6 +20,7 @@ import java.util.stream.Collectors;
 
 import static stock.price.analytics.model.prices.enums.StockTimeframe.*;
 import static stock.price.analytics.util.LoggingUtil.logTime;
+import static stock.price.analytics.util.PartitionAndSavePriceEntityUtil.partitionDataAndSave;
 import static stock.price.analytics.util.StockDateUtils.*;
 
 
@@ -155,7 +156,7 @@ public class PriceOHLCService {
                 .collect(Collectors.groupingBy(AbstractPriceOHLC::getTicker, Collectors.mapping(p -> (AbstractPriceOHLC) p, Collectors.toList())));
 
         List<AbstractPriceOHLC> updatedPrices = updatePricesAndPerformance(importedDailyPrices, timeframe, previousPricesByTicker);
-        priceOHLCRepository.saveAll(updatedPrices);
+        partitionDataAndSave(updatedPrices, priceOHLCRepository);
     }
 
     private List<AbstractPriceOHLC> updatePricesAndPerformance(List<DailyPriceOHLC> dailyPrices, StockTimeframe timeframe, Map<String, List<AbstractPriceOHLC>> previousTwoWMYPricesByTicker) {
