da0c3bac6ba201511154484c98ef13350bdce2dc
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-01-12 13:54:07 +0200

    TickerHighLowView projection for sql method

---
+++ src/main/java/stock/price/analytics/model/prices/highlow/TickerHighLowView.java
@@ -0,0 +1,10 @@
+package stock.price.analytics.model.prices.highlow;
+
+// query projection for updating high-low prices for different periods (4w, 52w, all-time)
+public interface TickerHighLowView {
+
+    String getTicker();
+    Double getLow();
+    Double getHigh();
+
+}
