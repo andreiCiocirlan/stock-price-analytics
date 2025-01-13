84fb12c7d3e4559dc156a46b3f1c38d089c4b551
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-01-13 14:15:56 +0200

    remove unused import

--- src/main/java/stock/price/analytics/repository/prices/DailyPricesJSONRepository.java
+++ src/main/java/stock/price/analytics/repository/prices/DailyPricesJSONRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import stock.price.analytics.model.prices.json.DailyPricesJSON;
-import stock.price.analytics.model.prices.ohlc.DailyPriceOHLC;
 
 import java.time.LocalDate;
 import java.util.List;
