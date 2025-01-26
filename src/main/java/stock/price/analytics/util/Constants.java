599f4a895a40762533b40182e78d999b13465fc8
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-01-27 01:21:45 +0200

    refactor and optimize partitionAndSave

--- src/main/java/stock/price/analytics/util/Constants.java
+++ src/main/java/stock/price/analytics/util/Constants.java
@@ -4,7 +4,6 @@ public class Constants {
 
     public static final String STOCKS_LOCATION = "D:\\stocks-data\\historical_prices\\ALL_STOCKS\\";
     public static final String FINNHUB_BASE_URL = "https://finnhub.io/api/v1";
-    public static final int NR_THREADS = 10;
     public static final String CLASSPATH_TICKERS_XTB_TXT = "classpath:tickers_XTB.txt";
     public static final String TIMEFRAME_PATTERN = "^(DAILY|WEEKLY|MONTHLY|YEARLY)$";
     public static final String HIGHER_TIMEFRAMES_PATTERN = "^[WMQYwmqy]{1,4}$";
--- src/main/java/stock/price/analytics/util/PartitionAndSavePriceEntityUtil.java
+++ src/main/java/stock/price/analytics/util/PartitionAndSavePriceEntityUtil.java
@@ -7,12 +7,6 @@ import stock.price.analytics.model.prices.PriceEntity;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
-
-import static stock.price.analytics.util.Constants.NR_THREADS;
 
 @Slf4j
 @Component
@@ -42,20 +36,15 @@ public class PartitionAndSavePriceEntityUtil {
     }
 
     private static <T, R extends PriceEntity> void save(List<List<T>> partitions, JpaRepository<R, Long> repository) {
-        List<Callable<Void>> callables = partitions.stream().map(sublist ->
-                (Callable<Void>) () -> {
-                    @SuppressWarnings("unchecked")
-                    List<R> rs = (List<R>) sublist;
-                    repository.saveAll(rs);
-                    return null;
-                }).collect(Collectors.toList());
-        ExecutorService executorService = Executors.newFixedThreadPool(NR_THREADS);
-        try {
-            executorService.invokeAll(callables);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            executorService.shutdown();
+        for (List<T> partition : partitions) {
+            try {
+                List<R> rs = (List<R>) partition;
+                rs.forEach(System.out::println);
+                repository.saveAllAndFlush(rs);
+            } catch (Exception e) {
+                log.error("Error saving partition: {}", e.getMessage(), e);
+                throw e; // Re-throw to stop execution if save fails
+            }
         }
     }
 
