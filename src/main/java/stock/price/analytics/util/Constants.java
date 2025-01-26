ceee1d924cb1b70d925b531612261427af7bed2b
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-01-26 14:01:28 +0200

    add quarterly timeframe option

--- src/main/java/stock/price/analytics/util/Constants.java
+++ src/main/java/stock/price/analytics/util/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
     public static final int NR_THREADS = 10;
     public static final String CLASSPATH_TICKERS_XTB_TXT = "classpath:tickers_XTB.txt";
     public static final String TIMEFRAME_PATTERN = "^(DAILY|WEEKLY|MONTHLY|YEARLY)$";
-    public static final String HIGHER_TIMEFRAMES_PATTERN = "^[WMYwmy]{1,3}$";
+    public static final String HIGHER_TIMEFRAMES_PATTERN = "^[WMQYwmqy]{1,3}$";
     public static final int MAX_TICKER_COUNT_PRINT = 100;
 }
