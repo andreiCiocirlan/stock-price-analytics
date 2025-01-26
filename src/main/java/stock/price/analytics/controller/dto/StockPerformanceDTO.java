f95ad4937b2372f573148fefd08ef4dfef8cbbbc
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-01-26 16:38:18 +0200

    round 2 decimals for the performance in the canonical constructor

--- src/main/java/stock/price/analytics/controller/dto/StockPerformanceDTO.java
+++ src/main/java/stock/price/analytics/controller/dto/StockPerformanceDTO.java
@@ -1,5 +1,7 @@
 package stock.price.analytics.controller.dto;
 
 public record StockPerformanceDTO(String ticker, double performance) {
-
+    public StockPerformanceDTO {
+        performance = Math.round(performance * 100.0) / 100.0; // round to 2 decimals
+    }
 }
