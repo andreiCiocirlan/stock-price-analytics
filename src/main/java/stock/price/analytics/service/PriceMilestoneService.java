5e0c2c786a5f01c784eb33786ef692ae03bd3fdb
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-01-31 23:05:28 +0200

    replace SQL query with priceMilestoneCache logic

--- src/main/java/stock/price/analytics/service/PriceMilestoneService.java
+++ src/main/java/stock/price/analytics/service/PriceMilestoneService.java
@@ -1,50 +1,20 @@
 package stock.price.analytics.service;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import stock.price.analytics.cache.PriceMilestoneCache;
 import stock.price.analytics.model.prices.enums.PriceMilestone;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAdjusters;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PriceMilestoneService {
 
-    @PersistenceContext
-    private final EntityManager entityManager;
+    private final PriceMilestoneCache priceMilestoneCache;
 
-    @SuppressWarnings("unchecked")
     public List<String> findTickersForMilestone(PriceMilestone priceMilestone, double cfdMargin) {
-        String queryStr = queryPriceMilestone(priceMilestone, cfdMargin);
-        Query nativeQuery = entityManager.createNativeQuery(queryStr, String.class);
-
-        return (List<String>) nativeQuery.getResultList();
+        return priceMilestoneCache.findTickersForMilestone(priceMilestone, cfdMargin);
     }
 
-    private String queryPriceMilestone(PriceMilestone priceMilestone, double cfdMargin) {
-        String highLowTable = priceMilestone.tableName();
-        String whereClause = priceMilestone.whereClause();
-        String currentMondayFormatted = LocalDate.now().with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
-                .format(DateTimeFormatter.ISO_LOCAL_DATE);
-        String joinDateFormatted = priceMilestone.joinDate();
-
-        return STR."""
-                SELECT wp.ticker
-                FROM weekly_prices wp
-                JOIN \{highLowTable} hl ON hl.ticker = wp.ticker
-                    AND hl.start_date = '\{joinDateFormatted}'
-                JOIN stocks s ON s.ticker = wp.ticker
-                WHERE
-                	wp.start_date = '\{currentMondayFormatted}'
-                	AND \{whereClause}
-                	AND s.cfd_margin = \{cfdMargin};
-                """;
-    }
 }
