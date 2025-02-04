03365123607109fe07fed48482fe960a587955c1
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-04 18:01:28 +0200

    show pre-market dropdown options only during pre-market hours, hide otherwise

--- src/main/resources/static/js/stock-performance.js
+++ src/main/resources/static/js/stock-performance.js
@@ -1,5 +1,33 @@
 let currentTimeFrame = 'WEEKLY';
 let stockPerformanceChart;
+window.onload = showHidePreMarketOptions;
+setInterval(showHidePreMarketOptions, 300000); // poll every 5 minute to show/hide GAP_* premarket options
+
+function showHidePreMarketOptions() {
+    const selectElement = document.getElementById('priceMilestone');
+    const options = selectElement.options;
+
+    // Get current time in New York
+    const now = new Date();
+    const nyTimeString = now.toLocaleString("en-US", { timeZone: "America/New_York" });
+    const nyTime = new Date(nyTimeString);
+    const day = nyTime.getDay(); // 0 (Sun) - 6 (Sat)
+    const hour = nyTime.getHours();
+    const minute = nyTime.getMinutes();
+
+    // Check if it's Mon-Fri (1-5) and between 8:00 - 9:30 AM (NY time)
+    const isWeekday = day >= 1 && day <= 5;
+    const isTimeInRange = hour === 8 || (hour === 9 && minute < 30);
+
+    if (!(isWeekday && isTimeInRange)) {
+        for (let i = 0; i < options.length; i++) {
+            const optionValue = options[i].value;
+            if (optionValue.startsWith('GAP_')) {
+                options[i].style.display = 'none';
+            }
+        }
+    }
+}
 
 function dispatchTimeFrameChangeEvent() {
     currentTimeFrame = determineSelectedTimeFrame();
