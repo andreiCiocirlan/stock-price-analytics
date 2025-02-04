389d8232011675303e1c99ed0135ad49eadc60ac
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-04 15:51:12 +0200

    PreMarketPriceMilestone

---
+++ src/main/java/stock/price/analytics/model/prices/enums/PreMarketPriceMilestone.java
@@ -0,0 +1,10 @@
+package stock.price.analytics.model.prices.enums;
+
+public enum PreMarketPriceMilestone {
+    GAP_UP,
+    GAP_DOWN,
+    GAP_UP_10_PERCENT,
+    GAP_DOWN_10_PERCENT,
+    NONE;
+
+}
