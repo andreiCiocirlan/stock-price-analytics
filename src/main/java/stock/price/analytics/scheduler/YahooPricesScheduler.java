d812697eb09fb0af02e0d8b56ace98f3b576424d
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-04 16:08:34 +0200

    log only size instead of entire response body

--- src/main/java/stock/price/analytics/scheduler/YahooPricesScheduler.java
+++ src/main/java/stock/price/analytics/scheduler/YahooPricesScheduler.java
@@ -13,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 import stock.price.analytics.model.prices.ohlc.DailyPrice;
 
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Component
@@ -26,10 +27,10 @@ public class YahooPricesScheduler {
     private final RestTemplate restTemplate;
 
     // Get Yahoo-API prices at 5 minutes past every hour from 9 AM to 4 PM (NY time) MON to FRI
-    @Scheduled(cron = "0 5 9-16 * * MON-FRI", zone = "America/New_York")
+    @Scheduled(cron = "0 15 9-16 * * MON-FRI", zone = "America/New_York")
     public void getYahooPrices() {
         String url = "http://localhost:" + serverPort + "/yahoo-prices/import"; // Use the injected port
         ResponseEntity<List<DailyPrice>> response = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
-        log.info("Yahoo API imported: {}", response.getBody());
+        log.info("Yahoo Prices Scheduler imported: {} daily prices", Objects.requireNonNull(response.getBody()).size());
     }
 }
