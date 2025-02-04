03c95143dcbb569eb9e29d97a200d321e09eb34d
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-04 13:15:38 +0200

    enable scheduling and get Yahoo-API prices at 5 minutes past every hour from 9 AM to 4 PM (NY time) MON to FRI

---
+++ src/main/java/stock/price/analytics/scheduler/YahooPricesScheduler.java
@@ -0,0 +1,35 @@
+package stock.price.analytics.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import stock.price.analytics.model.prices.ohlc.DailyPrice;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@EnableScheduling
+@RequiredArgsConstructor
+public class YahooPricesScheduler {
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    private final RestTemplate restTemplate;
+
+    // Get Yahoo-API prices at 5 minutes past every hour from 9 AM to 4 PM (NY time) MON to FRI
+    @Scheduled(cron = "0 5 9-16 * * MON-FRI", zone = "America/New_York")
+    public void getYahooPrices() {
+        String url = "http://localhost:" + serverPort + "/yahoo-prices/import"; // Use the injected port
+        ResponseEntity<List<DailyPrice>> response = restTemplate.exchange(url, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
+        log.info("Yahoo API imported: {}", response.getBody());
+    }
+}
