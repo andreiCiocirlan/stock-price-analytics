37e09f01cbc58837302a620648f4a2458dfdbd7f
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-09 02:18:45 +0200

    stomp.js, sock.js and websockets config

---
+++ src/main/java/stock/price/analytics/controller/AlertController.java
@@ -0,0 +1,18 @@
+package stock.price.analytics.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class AlertController {
+
+    @MessageMapping("/app/alert")
+    @SendTo("/topic/desktop-notifications")
+    public String desktopNotify(String message) {
+        log.info("Received Desktop Notification: {}", message);
+        return message;
+    }
+}
---
+++ src/main/resources/static/js/desktop-notifications.js
@@ -0,0 +1,36 @@
+var stompClient = null;
+
+function connect() {
+    var socket = new SockJS('/ws');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/desktop-notifications', function (message) {
+            showAlert(message.body);
+        });
+    });
+}
+
+function sendAlert(message) {
+    stompClient.send("/app/alert", {}, message);
+}
+
+function showAlert(message) {
+    if (Notification.permission === "granted") {
+        var notification = new Notification("Price Alert", { body: message });
+    } else if (Notification.permission !== "denied") {
+        console.log("Notification permission is not granted or denied yet.  Please click the button.");
+         Notification.requestPermission().then(function (permission) {
+            if (permission === "granted") {
+                console.log("Notification permission granted.");
+                var notification = new Notification("Price Alert", { body: message });
+            } else {
+                console.log("Notification permission denied.");
+            }
+        });
+    }
+}
+
+$(function () {
+    connect();
+});
