a26e83fe7d6eac6a3c7b2d15448863e477fcb374
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-09 13:16:55 +0200

    controller to test desktop notifications

---
+++ src/main/java/stock/price/analytics/controller/DesktopNotificationsController.java
@@ -0,0 +1,25 @@
+package stock.price.analytics.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import stock.price.analytics.service.DesktopNotificationService;
+
+@Slf4j
+@RequestMapping("/desktop-notification")
+@RestController
+@RequiredArgsConstructor
+public class DesktopNotificationsController {
+
+    private final DesktopNotificationService desktopNotificationService;
+
+    @PostMapping("/send")
+    public void sendDesktopNotification(@RequestParam String message) {
+        desktopNotificationService.broadcastDesktopNotification(message);
+        log.info("Desktop notification sent: {}", message);
+    }
+
+}
