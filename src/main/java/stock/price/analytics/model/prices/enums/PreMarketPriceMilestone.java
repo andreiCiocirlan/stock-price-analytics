8d7f5bb5c3787c69c870529471e75b24358af5fb
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-10 16:03:12 +0200

    toString added

--- src/main/java/stock/price/analytics/model/prices/enums/PreMarketPriceMilestone.java
+++ src/main/java/stock/price/analytics/model/prices/enums/PreMarketPriceMilestone.java
@@ -1,10 +1,34 @@
 package stock.price.analytics.model.prices.enums;
 
 public enum PreMarketPriceMilestone {
-    GAP_UP,
-    GAP_DOWN,
-    GAP_UP_10_PERCENT,
-    GAP_DOWN_10_PERCENT,
-    NONE;
-
+    GAP_UP {
+        @Override
+        public String toString() {
+            return "Gap Up";
+        }
+    },
+    GAP_DOWN {
+        @Override
+        public String toString() {
+            return "Gap Down";
+        }
+    },
+    GAP_UP_10_PERCENT {
+        @Override
+        public String toString() {
+            return "Pre-Market: Up more than 10%";
+        }
+    },
+    GAP_DOWN_10_PERCENT {
+        @Override
+        public String toString() {
+            return "Pre-Market: Down more than 10%";
+        }
+    },
+    NONE {
+        @Override
+        public String toString() {
+            return "None";
+        }
+    };
 }
