ccb9212b96719b1a96dc5e1a80e14c10dea14bf0
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-10 21:23:42 +0200

    findTickersForMilestones for any Enum

--- src/main/java/stock/price/analytics/service/PriceMilestoneService.java
+++ src/main/java/stock/price/analytics/service/PriceMilestoneService.java
@@ -8,9 +8,7 @@ import stock.price.analytics.cache.PriceMilestoneCache;
 import stock.price.analytics.model.prices.enums.PreMarketPriceMilestone;
 import stock.price.analytics.model.prices.enums.PricePerformanceMilestone;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static stock.price.analytics.util.EnumParser.isNoneEnum;
 import static stock.price.analytics.util.EnumParser.parseEnumWithNoneValue;
@@ -22,6 +20,14 @@ public class PriceMilestoneService {
 
     private final PriceMilestoneCache priceMilestoneCache;
     private final PreMarketPriceMilestoneCache preMarketPriceMilestoneCache;
+
+    public Map<String, List<String>> findTickersForMilestones(List<Enum<? extends Enum<?>>> priceMilestones, List<Double> cfdMargins) {
+        Map<String, List<String>> tickersByPriceMilestones = new HashMap<>();
+        for (Enum<? extends Enum<?>> priceMilestone : priceMilestones) {
+            tickersByPriceMilestones.put(priceMilestone.toString(), findTickersForMilestone(priceMilestone.name(), cfdMargins));
+        }
+        return tickersByPriceMilestones;
+    }
 
     public List<String> findTickersForMilestone(String priceMilestone, List<Double> cfdMargins) {
         final List<String> tickers = new ArrayList<>();
