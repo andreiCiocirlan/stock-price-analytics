388b337fd9ec502ed63dcf1c97f197f876f8f913
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-10 20:06:19 +0200

    refactor and use @Schedules to group intraday and pre-market timings

--- src/main/java/stock/price/analytics/scheduler/YahooPricesImportScheduler.java
+++ src/main/java/stock/price/analytics/scheduler/YahooPricesImportScheduler.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,26 +25,22 @@ public class YahooPricesImportScheduler {
     private String serverPort;
 
     // executed at 9:35 NY time (5 minutes after market open)
-    @Scheduled(cron = "${cron.expression.yahoo.quotes.intraday.at935}", zone = "${cron.expression.timezone}")
-    public void yahooPricesIntradayAt935() {
-        callYahooPricesImport(INTRADAY_LOG_PREFIX);
-    }
-
     // executed at 5 and 35 minutes past the hour between 10-17 NY time (intraday)
-    @Scheduled(cron = "${cron.expression.yahoo.quotes.intraday.between10and17}", zone = "${cron.expression.timezone}")
-    public void yahooPricesIntradayBetween10and17() {
+    @Schedules({
+            @Scheduled(cron = "${cron.expression.yahoo.quotes.intraday.at935}", zone = "${cron.expression.timezone}"),
+            @Scheduled(cron = "${cron.expression.yahoo.quotes.intraday.between10and17}", zone = "${cron.expression.timezone}")
+    })
+    public void yahooPricesIntraday() {
         callYahooPricesImport(INTRADAY_LOG_PREFIX);
     }
 
     // executed at 8:00, 8:15, 8:30, 8:45 NY time (pre-market)
-    @Scheduled(cron = "${cron.expression.yahoo.quotes.pre.market.between8and9}", zone = "${cron.expression.timezone}")
-    public void yahooPricesPreMarketBetween8And9() {
-        callYahooPricesImport(PREMARKET_LOG_PREFIX);
-    }
-
     // executed at 9:00, 9:15 NY time (pre-market)
-    @Scheduled(cron = "${cron.expression.yahoo.quotes.pre.market.between9and915}", zone = "${cron.expression.timezone}")
-    public void yahooPricesPreMarketBetween9And915() {
+    @Schedules({
+            @Scheduled(cron = "${cron.expression.yahoo.quotes.pre.market.between8and9}", zone = "${cron.expression.timezone}"),
+            @Scheduled(cron = "${cron.expression.yahoo.quotes.pre.market.between9and915}", zone = "${cron.expression.timezone}")
+    })
+    public void yahooPricesPreMarket() {
         callYahooPricesImport(PREMARKET_LOG_PREFIX);
     }
 
