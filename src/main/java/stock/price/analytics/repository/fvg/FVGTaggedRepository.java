44c48e0c193ac9ff9f62a0a8c0f4fea9696f9d7e
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-11 15:32:02 +0200

    find FVGs tagged that are in the 95th percentile of the 52w_high or 52w_low

--- src/main/java/stock/price/analytics/repository/fvg/FVGTaggedRepository.java
+++ src/main/java/stock/price/analytics/repository/fvg/FVGTaggedRepository.java
@@ -34,6 +34,27 @@ public interface FVGTaggedRepository extends JpaRepository<FairValueGap, Long> {
             """, nativeQuery = true)
     List<FairValueGap> findWeeklyTaggedFVGsBullish95thPercentileAllTimeLow(@Param("cfdMargins") List<Double> cfdMargins);
 
+    @Query(value = """
+            SELECT fvg.*
+            FROM stocks s
+            JOIN fvg on fvg.ticker = s.ticker AND fvg.status = 'OPEN' AND fvg.timeframe = 'WEEKLY' and fvg.type = 'BEARISH'
+            WHERE
+                    s.cfd_margin in :cfdMargins
+                AND s.low52w <> s.high52w AND (1 - (1 - ((s.w_close - s.low52w) / (s.high52w - s.low52w)))) > 0.95
+                AND (s.w_high between fvg.low AND fvg.high OR s.w_low between fvg.low AND fvg.high)
+            """, nativeQuery = true)
+    List<FairValueGap> findWeeklyTaggedFVGsBearish95thPercentile52wHigh(@Param("cfdMargins") List<Double> cfdMargins);
 
+    @Query(value = """
+            SELECT fvg.*
+            FROM stocks s
+            JOIN fvg on fvg.ticker = s.ticker AND fvg.status = 'OPEN' AND fvg.timeframe = 'WEEKLY' and fvg.type = 'BULLISH'
+            WHERE
+                    s.cfd_margin in :cfdMargins
+            	AND s.low52w <> s.high52w AND (1 - (s.w_close - s.low52w) / (s.high52w - s.low52w)) > 0.95
+                AND fvg.date < date_trunc('WEEK', s.last_updated) - INTERVAL '1 week'
+                AND (s.w_high between fvg.low AND fvg.high OR s.w_low between fvg.low AND fvg.high)
+            """, nativeQuery = true)
+    List<FairValueGap> findWeeklyTaggedFVGsBullish95thPercentile52wLow(@Param("cfdMargins") List<Double> cfdMargins);
 
 }
