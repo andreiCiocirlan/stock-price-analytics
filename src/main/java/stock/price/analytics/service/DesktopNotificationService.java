25e62a7518e12af5d32d851aa6f3d467004d3e44
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-11 16:16:02 +0200

    log desktop notification message

--- src/main/java/stock/price/analytics/service/DesktopNotificationService.java
+++ src/main/java/stock/price/analytics/service/DesktopNotificationService.java
@@ -1,9 +1,11 @@
 package stock.price.analytics.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DesktopNotificationService {
@@ -11,6 +13,7 @@ public class DesktopNotificationService {
     private final SimpMessagingTemplate template;
 
     public void broadcastDesktopNotification(String message) {
+        log.info("{}", message);
         this.template.convertAndSend("/topic/desktop-notification", message);
     }
 
