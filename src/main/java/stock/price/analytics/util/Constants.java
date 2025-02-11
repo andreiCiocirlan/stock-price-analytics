302829eaeec2a5e65924d1a8ded780bd6b83a7e9
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-11 17:34:42 +0200

    add CFD_MARGINS constants to be used in schedulers

--- src/main/java/stock/price/analytics/util/Constants.java
+++ src/main/java/stock/price/analytics/util/Constants.java
@@ -1,5 +1,7 @@
 package stock.price.analytics.util;
 
+import java.util.List;
+
 public class Constants {
 
     public static final String STOCKS_LOCATION = "D:\\stocks-data\\historical_prices\\ALL_STOCKS\\";
@@ -14,4 +16,7 @@ public class Constants {
     public static final String BULLISH_FVG_95TH_PERCENTILE_4W = "Bullish FVG 95% 4w:";
     public static final String BULLISH_FVG_95TH_PERCENTILE_52W = "Bullish FVG 95% 52w:";
     public static final String BULLISH_FVG_95TH_PERCENTILE_ATH = "Bullish FVG 95% ATH:";
+    public static final List<Double> CFD_MARGINS_5X_4X = List.of(0.2, 0.25);
+    public static final List<Double> CFD_MARGINS_5X_4X_3X = List.of(0.2, 0.25, 0.33);
+    public static final List<Double> CFD_MARGINS_5X_4X_3X_2X = List.of(0.2, 0.25, 0.33, 0.5);
 }
