d74c9544fd8c10662dcaac5f805ad01544c5bb76
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-11 13:35:30 +0200

    add methods for higher timeframe prices with previous close

--- src/main/java/stock/price/analytics/cache/HigherTimeframePricesCache.java
+++ src/main/java/stock/price/analytics/cache/HigherTimeframePricesCache.java
@@ -1,6 +1,10 @@
 package stock.price.analytics.cache;
 
 import org.springframework.stereotype.Component;
+import stock.price.analytics.cache.model.MonthlyPriceWithPrevClose;
+import stock.price.analytics.cache.model.QuarterlyPriceWithPrevClose;
+import stock.price.analytics.cache.model.WeeklyPriceWithPrevClose;
+import stock.price.analytics.cache.model.YearlyPriceWithPrevClose;
 import stock.price.analytics.model.prices.ohlc.MonthlyPrice;
 import stock.price.analytics.model.prices.ohlc.QuarterlyPrice;
 import stock.price.analytics.model.prices.ohlc.WeeklyPrice;
@@ -21,14 +25,89 @@ public class HigherTimeframePricesCache {
     private final Map<String, QuarterlyPrice> quarterlyPricesByTickerAndDate = new HashMap<>();
     private final Map<String, YearlyPrice> yearlyPricesByTickerAndDate = new HashMap<>();
 
-    public void addWeeklyPrices(List<WeeklyPrice> weeklyPrices) {
-        weeklyPrices.forEach(price -> {
+    private final Map<String, WeeklyPriceWithPrevClose> weeklyPricesWithPrevCloseByTickerAndDate = new HashMap<>();
+    private final Map<String, MonthlyPriceWithPrevClose> monthlyPricesWithPrevCloseByTickerAndDate = new HashMap<>();
+    private final Map<String, QuarterlyPriceWithPrevClose> quarterlyPricesWithPrevCloseByTickerAndDate = new HashMap<>();
+    private final Map<String, YearlyPriceWithPrevClose> yearlyPricesWithPrevCloseByTickerAndDate = new HashMap<>();
+
+    public void addWeeklyPriceWithPrevClose(List<WeeklyPriceWithPrevClose> weeklyPricesWithPrevClose) {
+        weeklyPricesWithPrevClose.forEach(price -> {
             weeklyPricesWithPrevCloseByTickerAndDate.merge(
-                    createKey(price.getTicker(), price.getStartDate()),
+                    createKey(price.weeklyPrice().getTicker(), price.weeklyPrice().getStartDate()),
                     price,
                     (_, newPrice) -> newPrice // Logic to keep the latest price
             );
         });
+    }
+
+    public void addMonthlyPriceWithPrevClose(List<MonthlyPriceWithPrevClose> monthlyPricesWithPrevClose) {
+        monthlyPricesWithPrevClose.forEach(price -> {
+            monthlyPricesWithPrevCloseByTickerAndDate.merge(
+                    createKey(price.monthlyPrice().getTicker(), price.monthlyPrice().getStartDate()),
+                    price,
+                    (_, newPrice) -> newPrice // Logic to keep the latest price
+            );
+        });
+    }
+
+    public void addQuarterlyPriceWithPrevClose(List<QuarterlyPriceWithPrevClose> quarterlyPricesWithPrevClose) {
+        quarterlyPricesWithPrevClose.forEach(price -> {
+            quarterlyPricesWithPrevCloseByTickerAndDate.merge(
+                    createKey(price.quarterlyPrice().getTicker(), price.quarterlyPrice().getStartDate()),
+                    price,
+                    (_, newPrice) -> newPrice // Logic to keep the latest price
+            );
+        });
+    }
+
+    public void addYearlyPriceWithPrevClose(List<YearlyPriceWithPrevClose> yearlyPricesWithPrevClose) {
+        yearlyPricesWithPrevClose.forEach(price -> {
+            yearlyPricesWithPrevCloseByTickerAndDate.merge(
+                    createKey(price.yearlyPrice().getTicker(), price.yearlyPrice().getStartDate()),
+                    price,
+                    (_, newPrice) -> newPrice // Logic to keep the latest price
+            );
+        });
+    }
+
+    public List<WeeklyPriceWithPrevClose> weeklyPricesWithPrevCloseFor(List<String> tickers) {
+        return tickers.stream()
+                .flatMap(ticker ->
+                        weeklyPricesWithPrevCloseByTickerAndDate.entrySet().stream()
+                                .filter(entry -> entry.getKey().startsWith(ticker + "_"))
+                                .map(Map.Entry::getValue))
+                .collect(Collectors.toList());
+    }
+
+    public List<MonthlyPriceWithPrevClose> monthlyPricesWithPrevCloseFor(List<String> tickers) {
+        return tickers.stream()
+                .flatMap(ticker ->
+                        monthlyPricesWithPrevCloseByTickerAndDate.entrySet().stream()
+                                .filter(entry -> entry.getKey().startsWith(ticker + "_"))
+                                .map(Map.Entry::getValue))
+                .collect(Collectors.toList());
+    }
+
+    public List<QuarterlyPriceWithPrevClose> quarterlyPricesWithPrevCloseFor(List<String> tickers) {
+        return tickers.stream()
+                .flatMap(ticker ->
+                        quarterlyPricesWithPrevCloseByTickerAndDate.entrySet().stream()
+                                .filter(entry -> entry.getKey().startsWith(ticker + "_"))
+                                .map(Map.Entry::getValue))
+                .collect(Collectors.toList());
+    }
+
+    public List<YearlyPriceWithPrevClose> yearlyPricesWithPrevCloseFor(List<String> tickers) {
+        return tickers.stream()
+                .flatMap(ticker ->
+                        yearlyPricesWithPrevCloseByTickerAndDate.entrySet().stream()
+                                .filter(entry -> entry.getKey().startsWith(ticker + "_"))
+                                .map(Map.Entry::getValue))
+                .collect(Collectors.toList());
+    }
+
+
+    public void addWeeklyPrices(List<WeeklyPrice> weeklyPrices) {
         weeklyPrices.forEach(price ->
                 weeklyPricesByTickerAndDate.merge(
                         createKey(price.getTicker(), price.getStartDate()),
