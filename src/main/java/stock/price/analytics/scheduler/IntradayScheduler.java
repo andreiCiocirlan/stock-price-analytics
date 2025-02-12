7c98cacdaa3c5b0e0802c5ea1f26ce63df2553fc
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-12 12:14:24 +0200

    FVGs update Intraday and alert FVGs tagged afterward (10, 20, 30 sec based on type [4w, 52w, all-time])

---
+++ src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
@@ -0,0 +1,67 @@
+package stock.price.analytics.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import stock.price.analytics.model.prices.enums.PricePerformanceMilestone;
+import stock.price.analytics.model.prices.enums.StockTimeframe;
+import stock.price.analytics.service.DesktopNotificationService;
+import stock.price.analytics.service.FVGTaggedService;
+import stock.price.analytics.service.FairValueGapService;
+
+import java.util.List;
+
+import static stock.price.analytics.model.prices.enums.PricePerformanceMilestone.*;
+import static stock.price.analytics.util.Constants.*;
+
+@Component
+@RequiredArgsConstructor
+public class IntradayScheduler {
+
+    private final FairValueGapService fairValueGapService;
+    private final FVGTaggedService fvgTaggedService;
+    private final DesktopNotificationService desktopNotificationService;
+
+    @Scheduled(cron = "${cron.expression.intraday.fvg.update}", zone = "${cron.expression.timezone}")
+    public void updateFVGsAtIntraday() {
+        for (StockTimeframe timeframe : StockTimeframe.values()) {
+            fairValueGapService.findNewFVGsAndSaveFor(timeframe);
+            fairValueGapService.updateFVGsHighLowAndClosedFor(timeframe);
+        }
+    }
+
+    @Scheduled(cron = "${cron.expression.intraday.fvg.tagged.95th.percentile.4w}", zone = "${cron.expression.timezone}")
+    public void alertFVGsTagged95thPercentile4w() {
+        alertFVGsTaggedBy(List.of(HIGH_4W_95, LOW_4W_95));
+    }
+
+    @Scheduled(cron = "${cron.expression.intraday.fvg.tagged.95th.percentile.52w}", zone = "${cron.expression.timezone}")
+    public void alertFVGsTagged95thPercentile52w() {
+        alertFVGsTaggedBy(List.of(HIGH_52W_95, LOW_52W_95));
+    }
+
+    @Scheduled(cron = "${cron.expression.intraday.fvg.tagged.95th.percentile.ath}", zone = "${cron.expression.timezone}")
+    public void alertFVGsTagged95thPercentileATH() {
+        alertFVGsTaggedBy(List.of(HIGH_ALL_TIME_95, LOW_ALL_TIME_95));
+    }
+
+    private void alertFVGsTaggedBy(List<PricePerformanceMilestone> pricePerformanceMilestones) {
+        for (PricePerformanceMilestone priceMilestone : pricePerformanceMilestones) {
+            desktopNotificationService.broadcastDesktopNotification(alertPrefixFrom(priceMilestone) + fvgTaggedService.findWeeklyTaggedFVGsBy(priceMilestone, CFD_MARGINS_5X_4X));
+        }
+    }
+
+    public String alertPrefixFrom(PricePerformanceMilestone pricePerformanceMilestone) {
+        return switch (pricePerformanceMilestone) {
+            case NEW_ALL_TIME_HIGH, NEW_52W_HIGH, NEW_4W_HIGH, NEW_52W_LOW, NEW_4W_LOW, NEW_ALL_TIME_LOW, NONE ->
+                    throw new IllegalStateException("Unexpected value " + pricePerformanceMilestone.name());
+            case HIGH_52W_95 -> BEARISH_FVG_95TH_PERCENTILE_52W;
+            case HIGH_4W_95 -> BEARISH_FVG_95TH_PERCENTILE_4W;
+            case HIGH_ALL_TIME_95 -> BEARISH_FVG_95TH_PERCENTILE_ATH;
+            case LOW_52W_95 -> BULLISH_FVG_95TH_PERCENTILE_52W;
+            case LOW_4W_95 -> BULLISH_FVG_95TH_PERCENTILE_4W;
+            case LOW_ALL_TIME_95 -> BULLISH_FVG_95TH_PERCENTILE_ATH;
+        };
+    }
+
+}
