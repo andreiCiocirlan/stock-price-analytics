bcde05df3b3455278e7ea5f9f9e53a9282a7ffc4
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-16 15:51:06 +0200

    constant holding the oldest date of a DAILY FVG

--- src/main/java/stock/price/analytics/util/Constants.java
+++ src/main/java/stock/price/analytics/util/Constants.java
@@ -1,5 +1,6 @@
 package stock.price.analytics.util;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class Constants {
@@ -16,6 +17,7 @@ public class Constants {
     public static final String BULLISH_FVG_95TH_PERCENTILE_4W = "Bullish FVG 95% 4w:";
     public static final String BULLISH_FVG_95TH_PERCENTILE_52W = "Bullish FVG 95% 52w:";
     public static final String BULLISH_FVG_95TH_PERCENTILE_ATL = "Bullish FVG 95% ATL:";
+    public static final LocalDate DAILY_FVG_MIN_DATE = LocalDate.of(2022, 1, 31);
     public static final List<Double> CFD_MARGINS_5X_4X = List.of(0.2, 0.25);
     public static final List<Double> CFD_MARGINS_5X_4X_3X = List.of(0.2, 0.25, 0.33);
     public static final List<Double> CFD_MARGINS_5X_4X_3X_2X = List.of(0.2, 0.25, 0.33, 0.5);
