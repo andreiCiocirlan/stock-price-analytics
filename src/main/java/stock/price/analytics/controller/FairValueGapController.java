355ef7edd52077fa3ba89b6c9d2600dedfa3dcab
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-16 16:39:24 +0200

    remove unused import

--- src/main/java/stock/price/analytics/controller/FairValueGapController.java
+++ src/main/java/stock/price/analytics/controller/FairValueGapController.java
@@ -1,6 +1,5 @@
 package stock.price.analytics.controller;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
