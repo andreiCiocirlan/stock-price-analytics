f85c3091339b777996b445992c000a0c7e88ce81
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-17 17:40:38 +0200

    look for weekly opening price discrepancies at EODScheduler

--- src/main/java/stock/price/analytics/scheduler/EndOfDayScheduler.java
+++ src/main/java/stock/price/analytics/scheduler/EndOfDayScheduler.java
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import stock.price.analytics.service.DesktopNotificationService;
 import stock.price.analytics.service.FairValueGapService;
+import stock.price.analytics.service.PricesService;
 import stock.price.analytics.service.StockDiscrepanciesService;
 
 @Component
@@ -14,6 +15,7 @@ public class EndOfDayScheduler {
 
     private final FairValueGapService fairValueGapService;
     private final StockDiscrepanciesService stockDiscrepanciesService;
+    private final PricesService pricesService;
     private final DesktopNotificationService desktopNotificationService;
 
     // At the end of the trading day check if any stock discrepancies are found
@@ -24,6 +26,9 @@ public class EndOfDayScheduler {
         }
         if (!fairValueGapService.findFvgDateDiscrepancies().isEmpty()) {
             desktopNotificationService.broadcastDesktopNotification("FVG Date discrepancies found, check logs!");
+        }
+        if (!pricesService.findWeeklyOpeningPriceDiscrepancies().isEmpty()) {
+            desktopNotificationService.broadcastDesktopNotification("Weekly opening price discrepancies found, check logs!");
         }
     }
 
