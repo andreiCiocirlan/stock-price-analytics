3b8230397195612180c77b970a7a2402f05e0ec6
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-17 17:55:49 +0200

    add configuration flag for switching scheduling on-off and @ConditionalOnProperty

--- src/main/java/stock/price/analytics/config/SchedulingConfiguration.java
+++ src/main/java/stock/price/analytics/config/SchedulingConfiguration.java
@@ -1,10 +1,15 @@
 package stock.price.analytics.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@ConditionalOnProperty(
+        value="scheduling.enabled",
+        havingValue = "true",
+        matchIfMissing = true)
 public class SchedulingConfiguration {
     // This class exists only to enable scheduling.
 }
