eb7a86c05e12c33180e1c1ace036006128b991dc
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-22 15:51:59 +0200

    milestones95thPercentile

--- src/main/java/stock/price/analytics/model/prices/enums/PricePerformanceMilestone.java
+++ src/main/java/stock/price/analytics/model/prices/enums/PricePerformanceMilestone.java
@@ -1,5 +1,7 @@
 package stock.price.analytics.model.prices.enums;
 
+import java.util.List;
+
 public enum PricePerformanceMilestone implements PriceMilestone {
     NEW_52W_HIGH,
     NEW_4W_HIGH,
@@ -14,5 +16,9 @@ public enum PricePerformanceMilestone implements PriceMilestone {
     LOW_4W_95,
     LOW_ALL_TIME_95,
     NONE;
+
+    public static List<PricePerformanceMilestone> milestones95thPercentile() {
+        return List.of(HIGH_52W_95, HIGH_4W_95, HIGH_ALL_TIME_95, LOW_52W_95, LOW_4W_95, LOW_ALL_TIME_95);
+    }
 
 }
