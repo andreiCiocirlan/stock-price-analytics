1212e8c7d2a0fdb2b54ccbd68c2e5fb2da4df1e8
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-23 15:47:38 +0200

    getPrice() and implementation

--- src/main/java/stock/price/analytics/cache/model/PriceWithPrevClose.java
+++ src/main/java/stock/price/analytics/cache/model/PriceWithPrevClose.java
@@ -1,5 +1,8 @@
 package stock.price.analytics.cache.model;
 
+import stock.price.analytics.model.prices.ohlc.AbstractPrice;
+
 // Marker interface
 public interface PriceWithPrevClose {
+    AbstractPrice getPrice();
 }
--- src/main/java/stock/price/analytics/cache/model/WeeklyPriceWithPrevClose.java
+++ src/main/java/stock/price/analytics/cache/model/WeeklyPriceWithPrevClose.java
@@ -1,6 +1,11 @@
 package stock.price.analytics.cache.model;
 
+import stock.price.analytics.model.prices.ohlc.AbstractPrice;
 import stock.price.analytics.model.prices.ohlc.WeeklyPrice;
 
 public record WeeklyPriceWithPrevClose(WeeklyPrice weeklyPrice, double previousClose) implements PriceWithPrevClose {
+    @Override
+    public AbstractPrice getPrice() {
+        return weeklyPrice();
+    }
 }
