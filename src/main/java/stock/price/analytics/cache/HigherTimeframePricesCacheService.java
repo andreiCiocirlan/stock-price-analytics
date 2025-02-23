c1106f7b4308851e110b2344424f224ea9595f8b
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-23 14:48:34 +0200

    add priceWithPrevCloseFrom method to work in parallel with current cache

--- src/main/java/stock/price/analytics/cache/HigherTimeframePricesCacheService.java
+++ src/main/java/stock/price/analytics/cache/HigherTimeframePricesCacheService.java
@@ -2,11 +2,14 @@ package stock.price.analytics.cache;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import stock.price.analytics.cache.model.*;
 import stock.price.analytics.model.prices.enums.StockTimeframe;
-import stock.price.analytics.model.prices.ohlc.AbstractPrice;
+import stock.price.analytics.model.prices.ohlc.*;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +25,31 @@ public class HigherTimeframePricesCacheService {
             case QUARTERLY -> higherTimeframePricesCache.getQuarterlyPricesByTickerAndDate();
             case YEARLY -> higherTimeframePricesCache.getYearlyPricesByTickerAndDate();
         };
+    }
+
+    private <T extends AbstractPrice, R extends PriceWithPrevClose> List<R> priceWithPrevCloseFrom(List<T> previousThreePricesForTickers) {
+        Map<String, List<T>> previousTwoPricesByTicker = previousThreePricesForTickers
+                .stream()
+                .collect(Collectors.groupingBy(AbstractPrice::getTicker))
+                .values().stream()
+                .flatMap(prices -> prices.stream().sorted(Comparator.comparing(AbstractPrice::getStartDate).reversed()).limit(2))
+                .collect(Collectors.groupingBy(AbstractPrice::getTicker));
+        Map<String, Double> previousCloseByTicker = previousTwoPricesByTicker.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        entry -> entry.getValue().size() > 1 ? entry.getValue().get(1).getClose()
+                                : entry.getValue().getFirst().getOpen() // if IPO week, month, quarter, year -> take opening price
+                ));
+
+        return previousThreePricesForTickers.stream()
+                .map(price -> (R) switch (price.getTimeframe()) {
+                    case DAILY -> throw new IllegalStateException("Unexpected timeframe DAILY");
+                    case WEEKLY -> new WeeklyPriceWithPrevClose((WeeklyPrice) price, previousCloseByTicker.get(price.getTicker()));
+                    case MONTHLY -> new MonthlyPriceWithPrevClose((MonthlyPrice) price, previousCloseByTicker.get(price.getTicker()));
+                    case QUARTERLY -> new QuarterlyPriceWithPrevClose((QuarterlyPrice) price, previousCloseByTicker.get(price.getTicker()));
+                    case YEARLY -> new YearlyPriceWithPrevClose((YearlyPrice) price, previousCloseByTicker.get(price.getTicker()));
+                })
+                .toList();
     }
 
     public void addPrices(List<? extends AbstractPrice> prices) {
