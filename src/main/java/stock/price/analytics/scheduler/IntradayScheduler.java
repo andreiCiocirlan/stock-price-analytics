0d09e15f8bee37bbbbbcbe4c10e7f6b657278caa
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-02-25 15:08:15 +0200

    move fvgLabelFrom method to service layer

--- src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
+++ src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
@@ -13,8 +13,6 @@ import stock.price.analytics.service.FairValueGapService;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import static stock.price.analytics.model.prices.enums.FvgType.BEARISH;
-import static stock.price.analytics.model.prices.enums.FvgType.BULLISH;
 import static stock.price.analytics.model.prices.enums.PricePerformanceMilestone.*;
 import static stock.price.analytics.util.Constants.*;
 
@@ -37,35 +35,12 @@ public class IntradayScheduler {
         for (StockTimeframe timeframe : StockTimeframe.higherTimeframes()) {
             for (PricePerformanceMilestone priceMilestone : milestones95thPercentile()) {
                 for (FvgType fvgType : FvgType.values()) {
-                    String fvgLabel = fvgLabelFrom(priceMilestone, fvgType, timeframe);
+                    String fvgLabel = fairValueGapService.fvgLabelFrom(priceMilestone, fvgType, timeframe);
                     Set<String> fvgTaggedTickers = fvgTaggedService.findTickersFVGsTaggedFor(timeframe, fvgType, priceMilestone, cfdMargins54);
                     desktopNotificationService.broadcastDesktopNotification(fvgLabel + fvgTaggedTickers);
                 }
             }
         }
-    }
-
-    public String fvgLabelFrom(PricePerformanceMilestone pricePerformanceMilestone, FvgType fvgType, StockTimeframe stockTimeframe) {
-        String highLowTimeframeCorrelation = timeframeFrom(pricePerformanceMilestone);
-        boolean isLow95thPercentile = low95thPercentileValues().contains(pricePerformanceMilestone);
-        boolean isHigh95thPercentile = high95thPercentileValues().contains(pricePerformanceMilestone);
-        if (isLow95thPercentile && fvgType == BEARISH) {
-            // price near lower 95th percentile AND Bearish FVG
-            return String.join(" ", stockTimeframe.name(), "ANVIL", highLowTimeframeCorrelation, "FVGs:");
-        } else if (isHigh95thPercentile && fvgType == BULLISH) {
-            // price near upper 95th percentile AND Bullish FVG
-            return String.join(" ", stockTimeframe.name(), "ROCKET SHIP", highLowTimeframeCorrelation, "FVGs:");
-        }
-
-        String highLowLabel = isLow95thPercentile ? "Low" : "High";
-        String mainFvgLabel = switch (pricePerformanceMilestone) {
-            case HIGH_52W_95, LOW_52W_95 -> FVG_95TH_PERCENTILE_52W;
-            case HIGH_4W_95, LOW_4W_95 -> FVG_95TH_PERCENTILE_4W;
-            case HIGH_ALL_TIME_95, LOW_ALL_TIME_95 -> FVG_95TH_PERCENTILE_ALL_TIME;
-            case NEW_ALL_TIME_HIGH, NEW_52W_HIGH, NEW_4W_HIGH, NEW_52W_LOW, NEW_4W_LOW, NEW_ALL_TIME_LOW, NONE ->
-                    throw new IllegalStateException("Unexpected value " + pricePerformanceMilestone.name());
-        };
-        return String.join(" ", stockTimeframe.name(), fvgType.name(), mainFvgLabel, highLowLabel, "FVGs:");
     }
 
 }
--- src/main/java/stock/price/analytics/service/FairValueGapService.java
+++ src/main/java/stock/price/analytics/service/FairValueGapService.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import stock.price.analytics.model.fvg.FairValueGap;
 import stock.price.analytics.model.prices.enums.FvgStatus;
+import stock.price.analytics.model.prices.enums.FvgType;
+import stock.price.analytics.model.prices.enums.PricePerformanceMilestone;
 import stock.price.analytics.model.prices.enums.StockTimeframe;
 import stock.price.analytics.model.prices.ohlc.AbstractPrice;
 import stock.price.analytics.repository.fvg.FVGRepository;
@@ -14,6 +16,10 @@ import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static stock.price.analytics.model.prices.enums.FvgType.BEARISH;
+import static stock.price.analytics.model.prices.enums.FvgType.BULLISH;
+import static stock.price.analytics.model.prices.enums.PricePerformanceMilestone.*;
+import static stock.price.analytics.util.Constants.*;
 import static stock.price.analytics.util.LoggingUtil.logTimeAndReturn;
 import static stock.price.analytics.util.PartitionAndSavePriceEntityUtil.partitionDataAndSaveWithLogTime;
 
@@ -266,5 +272,28 @@ public class FairValueGapService {
 
     public List<Object[]> findFvgDateDiscrepancies() {
         return fvgRepository.findFvgDateDiscrepancies();
+    }
+
+    public String fvgLabelFrom(PricePerformanceMilestone pricePerformanceMilestone, FvgType fvgType, StockTimeframe stockTimeframe) {
+        String highLowTimeframeCorrelation = timeframeFrom(pricePerformanceMilestone);
+        boolean isLow95thPercentile = low95thPercentileValues().contains(pricePerformanceMilestone);
+        boolean isHigh95thPercentile = high95thPercentileValues().contains(pricePerformanceMilestone);
+        if (isLow95thPercentile && fvgType == BEARISH) {
+            // price near lower 95th percentile AND Bearish FVG
+            return String.join(" ", stockTimeframe.name(), "ANVIL", highLowTimeframeCorrelation, "FVGs:");
+        } else if (isHigh95thPercentile && fvgType == BULLISH) {
+            // price near upper 95th percentile AND Bullish FVG
+            return String.join(" ", stockTimeframe.name(), "ROCKET SHIP", highLowTimeframeCorrelation, "FVGs:");
+        }
+
+        String highLowLabel = isLow95thPercentile ? "Low" : "High";
+        String mainFvgLabel = switch (pricePerformanceMilestone) {
+            case HIGH_52W_95, LOW_52W_95 -> FVG_95TH_PERCENTILE_52W;
+            case HIGH_4W_95, LOW_4W_95 -> FVG_95TH_PERCENTILE_4W;
+            case HIGH_ALL_TIME_95, LOW_ALL_TIME_95 -> FVG_95TH_PERCENTILE_ALL_TIME;
+            case NEW_ALL_TIME_HIGH, NEW_52W_HIGH, NEW_4W_HIGH, NEW_52W_LOW, NEW_4W_LOW, NEW_ALL_TIME_LOW, NONE ->
+                    throw new IllegalStateException("Unexpected value " + pricePerformanceMilestone.name());
+        };
+        return String.join(" ", stockTimeframe.name(), fvgType.name(), mainFvgLabel, highLowLabel, "FVGs:");
     }
 }
