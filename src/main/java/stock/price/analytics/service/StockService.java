2c7d3ee3afd1f5ec125ff819cc9ac3c6fc977858
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-03 13:45:15 +0200

    no longer log delisted stocks

--- src/main/java/stock/price/analytics/service/StockService.java
+++ src/main/java/stock/price/analytics/service/StockService.java
@@ -153,7 +153,6 @@ public class StockService {
         List<Stock> stocksDelisted = new ArrayList<>();
         for (Stock stock : stocksCache.getStocksMap().values()) {
             if (stock.getLastUpdated().isBefore(LocalDate.now().minusDays(5))) {
-                log.warn("DELISTED stock {}", stock.getTicker());
                 stock.setDelistedDate(stock.getLastUpdated());
                 stocksDelisted.add(stock);
             }
