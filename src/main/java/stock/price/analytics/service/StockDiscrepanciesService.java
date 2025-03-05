1d07a91f4cecd80af19f5bb74d98b8a44c5ccd61
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-05 14:55:33 +0200

    refactor name

--- src/main/java/stock/price/analytics/service/StockDiscrepanciesService.java
+++ src/main/java/stock/price/analytics/service/StockDiscrepanciesService.java
@@ -51,8 +51,8 @@ public class StockDiscrepanciesService {
         return stocksWithDiscrepancies;
     }
 
-    private void logDiscrepancyAndAddToList(String ticker, String name, List<String> discrepanciesFound) {
-        String stockDiscrepancy = String.format("%s with %s discrepancy", ticker, name);
+    private void logDiscrepancyAndAddToList(String ticker, String discrepancyType, List<String> discrepanciesFound) {
+        String stockDiscrepancy = String.format("%s with %s discrepancy", ticker, discrepancyType);
         discrepanciesFound.add(stockDiscrepancy);
         log.warn("{}", stockDiscrepancy);
     }
