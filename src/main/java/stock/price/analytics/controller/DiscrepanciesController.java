dbed2b4b964951c927659ef7c7ec8a2c8f004a14
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-06 14:04:11 +0200

    Discrepancies controller

---
+++ src/main/java/stock/price/analytics/controller/DiscrepanciesController.java
@@ -0,0 +1,49 @@
+package stock.price.analytics.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import stock.price.analytics.model.prices.enums.HighLowPeriod;
+import stock.price.analytics.model.prices.enums.StockTimeframe;
+import stock.price.analytics.model.stocks.Stock;
+import stock.price.analytics.service.DiscrepanciesService;
+
+import java.util.List;
+
+@RequestMapping("/discrepancies")
+@RestController
+@RequiredArgsConstructor
+public class DiscrepanciesController {
+
+    private final DiscrepanciesService discrepanciesService;
+
+    @GetMapping("/stocks/find-all")
+    public List<String> findAllStockDiscrepancies() {
+        return discrepanciesService.findAllStocksDiscrepancies();
+    }
+
+    @GetMapping("/weekly-prices/find-all")
+    public List<String> findAllWeeklyPriceDiscrepancies() {
+        return discrepanciesService.findAllWeeklyPriceDiscrepancies();
+    }
+
+    @PutMapping("/stocks/high-low-for-period")
+    public void updateStocksWithHighLowDiscrepancy(@RequestParam(value = "period") HighLowPeriod period) {
+        discrepanciesService.updateStocksWithHighLow4wDiscrepancyFor(period);
+    }
+
+    @PutMapping("/weekly-prices/opening-prices")
+    public void updateWeeklyPricesWithOpeningPriceDiscrepancy() {
+        discrepanciesService.updateWeeklyPricesWithOpeningPriceDiscrepancy();
+    }
+
+    @GetMapping("/stocks/opening-price")
+    public List<Stock> findStocksWithOpeningPriceDiscrepancy(@RequestParam(value = "timeframe") StockTimeframe timeframe) {
+        return discrepanciesService.findStocksWithOpeningPriceDiscrepancyFor(timeframe);
+    }
+
+    @PutMapping("/stocks/opening-price")
+    public void updateStocksWithOpeningPriceDiscrepancyFor(@RequestParam(value = "timeframe") StockTimeframe timeframe) {
+        discrepanciesService.updateStocksWithOpeningPriceDiscrepancyFor(timeframe);
+    }
+
+}
