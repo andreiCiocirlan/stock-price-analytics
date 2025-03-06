55ae399294df4f74cd51b2d5c9b94e4d8613af3e
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-06 14:06:13 +0200

    use DiscrepanciesService

--- src/main/java/stock/price/analytics/scheduler/EndOfDayScheduler.java
+++ src/main/java/stock/price/analytics/scheduler/EndOfDayScheduler.java
@@ -4,33 +4,32 @@ package stock.price.analytics.scheduler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import stock.price.analytics.service.DesktopNotificationService;
-import stock.price.analytics.service.FairValueGapService;
-import stock.price.analytics.service.PricesDiscrepanciesService;
-import stock.price.analytics.service.StockDiscrepanciesService;
+import stock.price.analytics.service.*;
 
 @Component
 @RequiredArgsConstructor
 public class EndOfDayScheduler {
 
     private final FairValueGapService fairValueGapService;
-    private final StockDiscrepanciesService stockDiscrepanciesService;
-    private final PricesDiscrepanciesService pricesDiscrepanciesService;
+    private final DiscrepanciesService discrepanciesService;
     private final DesktopNotificationService desktopNotificationService;
 
     // At the end of the trading day check if any stock discrepancies are found
     @Scheduled(cron = "${cron.post.market.checks}", zone = "${cron.timezone}")
     public void findAllStockDiscrepanciesAtEOD() {
-        if (!stockDiscrepanciesService.findAllStockDiscrepancies().isEmpty()) {
-            desktopNotificationService.broadcastDesktopNotification("Stock Discrepancies found, check logs!");
+        if (!discrepanciesService.findStocksOpeningPriceDiscrepancies().isEmpty()) {
+            desktopNotificationService.broadcastDesktopNotification("Stocks Opening Price Discrepancies found, check logs!");
+        }
+        if (!discrepanciesService.findStocksHighLowsOrHTFDiscrepancies().isEmpty()) {
+            desktopNotificationService.broadcastDesktopNotification("Stocks High-Low/HTF Discrepancies found, check logs!");
         }
         if (!fairValueGapService.findFvgDateDiscrepancies().isEmpty()) {
             desktopNotificationService.broadcastDesktopNotification("FVG Date discrepancies found, check logs!");
         }
-        if (!pricesDiscrepanciesService.findWeeklyOpeningPriceDiscrepancies().isEmpty()) {
+        if (!discrepanciesService.findWeeklyOpeningPriceDiscrepancies().isEmpty()) {
             desktopNotificationService.broadcastDesktopNotification("Weekly opening price discrepancies found, check logs!");
         }
-        if (!pricesDiscrepanciesService.findWeeklyHighLowPriceDiscrepancies().isEmpty()) {
+        if (!discrepanciesService.findWeeklyHighLowPriceDiscrepancies().isEmpty()) {
             desktopNotificationService.broadcastDesktopNotification("Weekly High-Low price discrepancies found, check logs!");
         }
     }
