9bf8112327f4855078c9f06d943888ae07f33c52
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-08 15:23:43 +0200

    use current date instead of trading date (avoid pre-market logic for tradingDateNow)

--- src/main/java/stock/price/analytics/cache/util/ImportDateUtil.java
+++ src/main/java/stock/price/analytics/cache/util/ImportDateUtil.java
@@ -7,7 +7,6 @@ import stock.price.analytics.model.prices.enums.StockTimeframe;
 import java.time.LocalDate;
 
 import static stock.price.analytics.util.StockDateUtils.isWithinSameTimeframe;
-import static stock.price.analytics.util.TradingDateUtil.tradingDateNow;
 
 @Component
 @RequiredArgsConstructor
@@ -16,7 +15,7 @@ public class ImportDateUtil {
     public static boolean isFirstImportFor(StockTimeframe timeframe, LocalDate previousImportDate) {
         return switch (timeframe) {
             case DAILY -> throw new IllegalStateException("Unexpected value DAILY");
-            case WEEKLY, YEARLY, QUARTERLY, MONTHLY -> isWithinSameTimeframe(tradingDateNow(), previousImportDate, timeframe);
+            case WEEKLY, YEARLY, QUARTERLY, MONTHLY -> isWithinSameTimeframe(LocalDate.now(), previousImportDate, timeframe);
         };
     }
 
