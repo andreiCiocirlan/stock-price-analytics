a11edd60a4b662cbcf0ff9d54cd192038338dd72
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-09 21:33:50 +0200

    add NewTickerController with endpoint for importing all data for new tickers

---
+++ src/main/java/stock/price/analytics/controller/NewTickerController.java
@@ -0,0 +1,25 @@
+package stock.price.analytics.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import stock.price.analytics.service.NewTickerService;
+
+@Slf4j
+@RestController
+@RequestMapping("/new-ticker")
+@RequiredArgsConstructor
+public class NewTickerController {
+
+    private final NewTickerService newTickerService;
+
+    @PostMapping("/import-all-data")
+    public void newTickersimportAllDataFor(@RequestParam(value = "tickers") String tickers,
+                                 @RequestParam(value = "cfdMargin") Double cfdMargin,
+                                 @RequestParam(value = "shortSell") Boolean shortSell) {
+        newTickerService.importAllDataFor(tickers, cfdMargin, shortSell);
+    }
+}
