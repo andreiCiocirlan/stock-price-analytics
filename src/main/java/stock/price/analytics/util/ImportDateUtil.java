6a34460a2bdef672fd456519453946df461992b2
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-15 00:22:12 +0200

    use tradingDateNow instead

--- src/main/java/stock/price/analytics/util/ImportDateUtil.java
+++ src/main/java/stock/price/analytics/util/ImportDateUtil.java
@@ -7,6 +7,7 @@ import stock.price.analytics.model.prices.enums.StockTimeframe;
 import java.time.LocalDate;
 
 import static stock.price.analytics.util.StockDateUtils.isWithinSameTimeframe;
+import static stock.price.analytics.util.TradingDateUtil.tradingDateNow;
 
 @Component
 @RequiredArgsConstructor
@@ -15,7 +16,7 @@ public class ImportDateUtil {
     public static boolean isFirstImportFor(StockTimeframe timeframe, LocalDate previousImportDate) {
         return switch (timeframe) {
             case DAILY -> throw new IllegalStateException("Unexpected value DAILY");
-            case WEEKLY, YEARLY, QUARTERLY, MONTHLY -> !isWithinSameTimeframe(LocalDate.now(), previousImportDate, timeframe);
+            case WEEKLY, YEARLY, QUARTERLY, MONTHLY -> !isWithinSameTimeframe(tradingDateNow(), previousImportDate, timeframe);
         };
     }
 
