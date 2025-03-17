d395190505688199abb9c8af668ce58443d107db
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-18 01:30:50 +0200

    refactor savePriceGapsFor by writing query in java and parametrization

--- src/main/java/stock/price/analytics/service/PriceGapsService.java
+++ src/main/java/stock/price/analytics/service/PriceGapsService.java
@@ -1,23 +1,106 @@
 package stock.price.analytics.service;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import jakarta.websocket.Session;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import stock.price.analytics.model.prices.enums.StockTimeframe;
 import stock.price.analytics.repository.prices.gaps.PriceGapsRepository;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static stock.price.analytics.util.Constants.NY_ZONE;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PriceGapsService {
 
     private final PriceGapsRepository priceGapsRepository;
 
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    @Transactional
     public void saveAllPriceGapsFor(List<String> tickers) {
-        priceGapsRepository.saveDailyPriceGaps(tickers);
-        priceGapsRepository.saveWeeklyPriceGaps(tickers);
-        priceGapsRepository.saveMonthlyPriceGaps(tickers);
-        priceGapsRepository.saveQuarterlyPriceGaps(tickers);
-        priceGapsRepository.saveYearlyPriceGaps(tickers);
+        boolean allHistoricalData = true;
+        for (StockTimeframe timeframe : StockTimeframe.values()) {
+            savePriceGapsFor(tickers, timeframe, allHistoricalData);
+        }
+    }
+
+    @Transactional
+    public void savePriceGapsTodayFor(List<String> tickers, StockTimeframe timeframe) {
+        savePriceGapsFor(tickers, timeframe, false);
+    }
+
+    @Transactional
+    private void savePriceGapsFor(List<String> tickers, StockTimeframe timeframe, boolean allHistoricalData) {
+        String tickersFormatted = tickers.stream().map(ticker -> STR."'\{ticker}'").collect(Collectors.joining(", "));
+        String dbTable = timeframe.dbTableOHLC();
+        String dateColumn = timeframe == StockTimeframe.DAILY ? "date" : "start_date";
+        String dateTruncPeriod = timeframe.toDateTruncPeriod();
+        boolean isDailyTimeframeAndMonday = timeframe == StockTimeframe.DAILY && LocalDate.now(NY_ZONE).getDayOfWeek() == DayOfWeek.MONDAY;
+        int lookBackCount = isDailyTimeframeAndMonday ? 3 : 1;
+        if (allHistoricalData) {
+            lookBackCount = switch (timeframe) {
+                case DAILY -> 1000;
+                case WEEKLY -> 300;
+                case MONTHLY -> 200;
+                case QUARTERLY -> 100;
+                case YEARLY -> 10;
+            };
+        }
+
+        String query = STR."""
+            WITH max_date_cte AS (
+                select date_trunc('\{dateTruncPeriod}', (select max(last_updated) from stocks)) - INTERVAL '1 \{dateTruncPeriod}' as max_date
+            ),
+            ranked_prices AS (
+                SELECT
+                    ticker,
+                    close AS closing_price,
+                    \{dateColumn} AS closing_date,
+                    ROW_NUMBER() OVER (PARTITION BY ticker ORDER BY \{dateColumn} DESC) AS row_num
+                FROM \{dbTable}
+                WHERE ticker in (\{tickersFormatted})
+            	    AND ticker in (select ticker from stocks where cfd_margin in (0.2, 0.25, 0.33))
+                    AND \{dateColumn} between CURRENT_DATE - INTERVAL '\{lookBackCount} \{dateTruncPeriod}' and (SELECT max_date from max_date_cte)
+            ),
+            unfilled_gaps AS (
+                SELECT
+                    p1.ticker,
+                    p1.closing_price,
+                    p1.closing_date
+                FROM ranked_prices p1
+                WHERE row_num <= \{lookBackCount} and NOT EXISTS (
+                    SELECT 1
+                    FROM \{dbTable} p2
+                    WHERE p2.ticker = p1.ticker
+                    AND p2.\{dateColumn} > p1.closing_date
+                    AND p1.closing_price BETWEEN p2.low AND p2.high
+                )
+            )
+            INSERT INTO price_gaps (id, ticker, close, timeframe, status, date)
+            SELECT
+            	nextval('sequence_prices_gaps') AS id,
+            	ticker,
+            	closing_price,
+            	'\{timeframe}',
+            	'OPEN',
+            	closing_date
+            FROM unfilled_gaps;
+            """;
+
+        log.info("QUERY {}", query);
+        int rowsAffected = entityManager.createNativeQuery(query).executeUpdate();
+        log.info("saved {} rows for {} price gaps", rowsAffected, timeframe);
     }
 
     public void closePriceGaps() {
