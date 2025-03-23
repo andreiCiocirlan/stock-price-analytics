4d684d6a1ccfe1b4ced618b62554b4103d82f639
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-23 20:49:21 +0200

    remove unused constant

--- src/main/java/stock/price/analytics/util/Constants.java
+++ src/main/java/stock/price/analytics/util/Constants.java
@@ -13,7 +13,6 @@ public class Constants {
     public static final String STOCKS_LOCATION = "D:\\stocks-data\\historical_prices\\ALL_STOCKS\\";
     public static final String FINNHUB_BASE_URL = "https://finnhub.io/api/v1";
     public static final String CLASSPATH_TICKERS_XTB_TXT = "classpath:tickers_XTB.txt";
-    public static final String TIMEFRAME_PATTERN = "^(DAILY|WEEKLY|MONTHLY|YEARLY)$";
     public static final String HIGHER_TIMEFRAMES_PATTERN = "^[WMQYwmqy]{1,4}$";
     public static final int MAX_TICKER_COUNT_PRINT = 100;
     public static final String FVG_95TH_PERCENTILE_4W = "95% 4w";
