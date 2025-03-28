efb0d23ea5ef502235f0db9f4dad13275fc6a8ac
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-28 12:29:54 +0200

    use DynamicUpdate at entity level

--- src/main/java/stock/price/analytics/model/prices/gaps/PriceGap.java
+++ src/main/java/stock/price/analytics/model/prices/gaps/PriceGap.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
 import stock.price.analytics.model.BusinessEntity;
 import stock.price.analytics.model.prices.enums.GapStatus;
 import stock.price.analytics.model.prices.enums.StockTimeframe;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Entity
+@DynamicUpdate
 @NoArgsConstructor
 @Table(name = "price_gaps")
 public class PriceGap implements BusinessEntity {
