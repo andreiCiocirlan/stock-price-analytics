f6debe1ae1da88198bbb6d6da50c942234af3fbf
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-29 13:16:25 +0200

    remove unused readTickersXTB, writeToFile method

--- src/main/java/stock/price/analytics/util/FileUtils.java
+++ src/main/java/stock/price/analytics/util/FileUtils.java
@@ -1,31 +1,16 @@
 package stock.price.analytics.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.util.ResourceUtils;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-
-import static stock.price.analytics.util.Constants.CLASSPATH_TICKERS_XTB_TXT;
 
 @Slf4j
 public class FileUtils {
 
-    public static List<String> readTickersXTB() throws IOException {
-        return new ArrayList<>(Files.readAllLines(Paths.get(ResourceUtils.getFile(CLASSPATH_TICKERS_XTB_TXT).getAbsolutePath())));
-    }
-
-    public static void writeToFile(List<Object> objectList, File outputFile) {
-        try (PrintWriter pw = new PrintWriter(outputFile)) {
-            objectList.forEach(pw::println);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public static void writeToFile(String filePath, String jsonData) {
         try {
