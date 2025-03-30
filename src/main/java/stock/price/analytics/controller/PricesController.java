c213b8d66ef07352bd7434201724d78fe3f3f83d
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-30 21:43:34 +0300

    remove updateHigherTimeframesForDate

--- src/main/java/stock/price/analytics/controller/PricesController.java
+++ src/main/java/stock/price/analytics/controller/PricesController.java
@@ -1,18 +1,15 @@
 package stock.price.analytics.controller;
 
-import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import stock.price.analytics.controller.dto.CandleWithDateDTO;
 import stock.price.analytics.model.prices.enums.StockTimeframe;
 import stock.price.analytics.service.PricesService;
 
-import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
-
-import static stock.price.analytics.util.Constants.HIGHER_TIMEFRAMES_PATTERN;
 
 @RequestMapping("/ohlc")
 @RestController
@@ -24,16 +21,6 @@ public class PricesController {
     @GetMapping("/prices")
     public List<CandleWithDateDTO> pricesFor(@RequestParam("ticker") String ticker, @RequestParam("timeFrame") String timeFrame) {
         return pricesService.findFor(ticker, StockTimeframe.valueOf(timeFrame.toUpperCase()));
-    }
-
-    @PostMapping("/update-higher-timeframes")
-    public void updateHigherTimeframesForDate(@RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date,
-                                              @RequestParam(required = false, name = "timeFrames") @Pattern(regexp = HIGHER_TIMEFRAMES_PATTERN, message = "Invalid timeframe") String timeFrames,
-                                              @RequestParam(required = false, value = "tickers") String tickers) {
-        String tickersQueryParam = tickers != null ? STR."'\{tickers.replace(",", "','")}'" : null;
-        List<StockTimeframe> timeframes = timeFrames != null ?
-                StockTimeframe.valuesFromLetters(timeFrames.toUpperCase()) : Arrays.stream(StockTimeframe.values()).toList();
-        pricesService.updateHigherTimeframesPricesFor(date, timeframes, tickersQueryParam);
     }
 
 }
