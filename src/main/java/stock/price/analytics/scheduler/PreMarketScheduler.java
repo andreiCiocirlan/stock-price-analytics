b12ef07816263c4d3a548024036e6cac87281572
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-03-30 19:46:10 +0300

    init caches 5 mins before pre-market

--- src/main/java/stock/price/analytics/scheduler/PreMarketScheduler.java
+++ src/main/java/stock/price/analytics/scheduler/PreMarketScheduler.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Component;
+import stock.price.analytics.cache.CacheInitializationService;
 import stock.price.analytics.service.DesktopNotificationService;
 import stock.price.analytics.service.PriceMilestoneService;
 
@@ -16,6 +17,7 @@ public class PreMarketScheduler {
 
     private final PriceMilestoneService priceMilestoneService;
     private final DesktopNotificationService desktopNotificationService;
+    private final CacheInitializationService cacheInitializationService;
 
     @Schedules({
             @Scheduled(cron = "${cron.pre.market.alert.between8and9}", zone = "${cron.timezone}"),  // 10 15,30,45 8 * * MON-FRI
@@ -24,6 +26,11 @@ public class PreMarketScheduler {
     public void alertPreMarketGaps_moreThan_10Percent() {
         priceMilestoneService.findTickersForMilestones(preMarketSchedulerValues(), CFD_MARGINS_5X_4X_3X)
                 .forEach((priceMilestone, tickers) -> desktopNotificationService.broadcastDesktopNotification("Pre-Market alert", String.join(" ", priceMilestone.toString(), tickers.toString())));
+    }
+
+    @Scheduled(cron = "${cron.pre.market.initialize.caches}", zone = "${cron.timezone}")  // 10 15,30,45 8 * * MON-FRI
+    public void initializeAllCaches() {
+        cacheInitializationService.initializeAllCaches();
     }
 
 }
