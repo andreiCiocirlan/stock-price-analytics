1a1bc94d9edb7d488ece8ce458f4cea01e61ef65
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-04-11 12:14:03 +0300

    refactor

--- src/main/java/stock/price/analytics/model/prices/enums/SimpleMovingAverageMilestone.java
+++ src/main/java/stock/price/analytics/model/prices/enums/SimpleMovingAverageMilestone.java
@@ -2,33 +2,35 @@ package stock.price.analytics.model.prices.enums;
 
 import stock.price.analytics.model.prices.PriceMilestone;
 
+import java.util.List;
+
 public enum SimpleMovingAverageMilestone implements PriceMilestone {
 
-    ABOVE_200_SMA("sma_milestone") {
+    ABOVE_200_SMA("sma-milestone") {
         @Override
         public String toString() {
             return "> 200 SMA";
         }
     },
-    ABOVE_50_SMA("sma_milestone") {
+    ABOVE_50_SMA("sma-milestone") {
         @Override
         public String toString() {
             return "> 50 SMA";
         }
     },
-    BELOW_200_SMA("sma_milestone") {
+    BELOW_200_SMA("sma-milestone") {
         @Override
         public String toString() {
             return "< 200 SMA";
         }
     },
-    BELOW_50_SMA("sma_milestone") {
+    BELOW_50_SMA("sma-milestone") {
         @Override
         public String toString() {
             return "< 50 SMA";
         }
     },
-    NONE("sma_milestone") {
+    NONE("sma-milestone") {
         @Override
         public String toString() {
             return "None";
@@ -44,5 +46,9 @@ public enum SimpleMovingAverageMilestone implements PriceMilestone {
     @Override
     public String getType() {
         return this.type;
+    }
+
+    public static List<PriceMilestone> smaMilestones() {
+        return List.of(ABOVE_50_SMA, ABOVE_200_SMA, BELOW_50_SMA, BELOW_200_SMA);
     }
 }
