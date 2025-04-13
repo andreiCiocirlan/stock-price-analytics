4aa0468dfdf83142b7277bb83e992efcd2592f3b
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-04-13 23:40:43 +0300

    redo factor again

--- src/main/resources/static/js/stock-performance.js
+++ src/main/resources/static/js/stock-performance.js
@@ -162,7 +162,7 @@ function updateStockPerformanceChartWithData(data, timeFrame, numRows, numCols, 
                 color: '#FFFFFF',
                 formatter: function() {
                     const squareSize = Math.max(this.point.shapeArgs.width, this.point.shapeArgs.height);
-                    const fontSize = `${Math.min(11.5, squareSize * 0.2)}px`; // Adjust the scaling factor as needed
+                    const fontSize = `${Math.max(11.5, squareSize * 0.1)}px`; // Adjust the scaling factor as needed
                     return `<span style="font-size: ${fontSize};">${data[this.point.index].ticker}<br>${data[this.point.index].performance}%</span>`;
                 }
             },
