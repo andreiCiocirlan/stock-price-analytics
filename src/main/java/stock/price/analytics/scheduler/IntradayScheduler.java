250a4e94018e95ed245ac8593a835ffd0f46a576
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-04-15 19:36:15 +0300

    remove alerting milestones, to be done from UI instead

--- src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
+++ src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
@@ -2,7 +2,6 @@ package stock.price.analytics.scheduler;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Component;
 import stock.price.analytics.cache.CacheService;
 import stock.price.analytics.service.DesktopNotificationService;
@@ -25,13 +24,5 @@ public class IntradayScheduler {
         priceGapService.closePriceGaps();
     }
 
-    @Schedules({
-        @Scheduled(cron = "${cron.intraday.ticker.spikes.between10and16}", zone = "${cron.timezone}"), // 20 15,35,55 10-15 * * MON-FRI
-        @Scheduled(cron = "${cron.intraday.ticker.spikes.between16and17}", zone = "${cron.timezone}")  // 20 15,35 16 * * MON-FRI
-    })
-    public void alertIntradayPriceSpikes() {
-        cacheService.tickersByPriceMilestones()
-                .forEach((priceMilestone, tickers) -> desktopNotificationService.broadcastDesktopNotification(priceMilestone.toString(), tickers.toString()));
-    }
 
 }
