ca3e4131871e13ad3d208a4f20622983570a1c02
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-04-16 18:35:22 +0300

    any instead

--- src/main/java/stock/price/analytics/model/candlestick/CandleStickType.java
+++ src/main/java/stock/price/analytics/model/candlestick/CandleStickType.java
@@ -2,6 +2,6 @@ package stock.price.analytics.model.candlestick;
 
 public enum CandleStickType {
 
-    DOJI, DRAGONFLY_DOJI, GRAVESTONE_DOJI, INVERTED_HAMMER, NONE;
+    DOJI, DRAGONFLY_DOJI, GRAVESTONE_DOJI, INVERTED_HAMMER, ANY;
 
 }
