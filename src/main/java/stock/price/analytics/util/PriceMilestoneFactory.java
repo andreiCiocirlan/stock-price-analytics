2524acb9a0cd6bd70a39d5e603b82e0638e0c21f
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-04-20 00:47:42 +0300

    add PriceMilestoneFactory for ease of use

---
+++ src/main/java/stock/price/analytics/util/PriceMilestoneFactory.java
@@ -0,0 +1,47 @@
+package stock.price.analytics.util;
+
+import stock.price.analytics.model.prices.PriceMilestone;
+import stock.price.analytics.model.prices.enums.*;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class PriceMilestoneFactory {
+
+    private static final Map<String, PriceMilestone> PRICE_MILESTONE_REGISTRY = createRegistry();
+
+    private static Map<String, PriceMilestone> createRegistry() {
+        return Stream.of(
+                        PricePerformanceMilestone.values(),
+                        NewHighLowMilestone.values(),
+                        IntradayPriceSpike.values(),
+                        PreMarketPriceMilestone.values(),
+                        SimpleMovingAverageMilestone.values()
+                )
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toMap(
+                        Enum::name,
+                        Function.identity()
+                ));
+    }
+
+    public static List<PriceMilestone> priceMilestonesFrom(List<String> milestoneCodes) {
+        return milestoneCodes.stream()
+                .map(PriceMilestoneFactory::priceMilestoneFrom)
+                .toList();
+    }
+
+    private static PriceMilestone priceMilestoneFrom(String milestoneCode) {
+        PriceMilestone milestone = PRICE_MILESTONE_REGISTRY.get(milestoneCode);
+        if (milestone == null) {
+            throw new IllegalArgumentException("Invalid milestone milestoneCode: " + milestoneCode);
+        }
+        return milestone;
+    }
+
+    public static List<PriceMilestone> registry() {
+        return new ArrayList<>(PRICE_MILESTONE_REGISTRY.values());
+    }
+}
