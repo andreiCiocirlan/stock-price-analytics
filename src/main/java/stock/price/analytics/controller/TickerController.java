1f32dfd0e4de70215b3c91a43119246796aa638b
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-04-19 15:59:05 +0300

    delete-data-for-ticker endpoint

--- src/main/java/stock/price/analytics/controller/TickerController.java
+++ src/main/java/stock/price/analytics/controller/TickerController.java
@@ -2,10 +2,8 @@ package stock.price.analytics.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import stock.price.analytics.service.DeleteTickerService;
 import stock.price.analytics.service.NewTickerService;
 
 @Slf4j
@@ -15,11 +13,17 @@ import stock.price.analytics.service.NewTickerService;
 public class TickerController {
 
     private final NewTickerService newTickerService;
+    private final DeleteTickerService deleteTickerService;
 
     @PostMapping("/import-all-data")
     public void importAllDataFor(@RequestParam(value = "tickers") String tickers,
                                  @RequestParam(value = "cfdMargin") Double cfdMargin,
                                  @RequestParam(value = "shortSell") Boolean shortSell) {
         newTickerService.importAllDataFor(tickers, cfdMargin, shortSell);
+    }
+
+    @DeleteMapping("/delete-data-for-ticker")
+    public void deleteAllDataFor(@RequestParam(value = "ticker") String ticker) {
+        deleteTickerService.deleteAllDataFor(ticker);
     }
 }
