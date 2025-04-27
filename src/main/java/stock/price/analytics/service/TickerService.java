9137b88386b9538dc9210cb65edd78943bb56307
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-04-27 15:53:17 +0300

    move DB_TABLES to Constants

--- src/main/java/stock/price/analytics/service/TickerService.java
+++ src/main/java/stock/price/analytics/service/TickerService.java
@@ -6,8 +6,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
+import stock.price.analytics.util.Constants;
 
 @Slf4j
 @Service
@@ -17,24 +16,9 @@ public class TickerService {
     @PersistenceContext
     private final EntityManager entityManager;
 
-    private static final List<String> DB_TABLES = List.of(
-            "daily_prices_json",
-            "daily_prices",
-            "weekly_prices",
-            "monthly_prices",
-            "quarterly_prices",
-            "yearly_prices",
-            "stocks",
-            "price_gaps",
-            "fvg",
-            "high_low4w",
-            "high_low52w",
-            "highest_lowest"
-    );
-
     @Transactional
     public void renameTicker(String oldTicker, String newTicker) {
-        for (String table : DB_TABLES) {
+        for (String table : Constants.DB_TABLES) {
             String column = "daily_prices_json".equals(table) ? "symbol" : "ticker";
             String sql = STR."UPDATE \{table} SET \{column} = ?2 WHERE \{column} = ?1";
 
@@ -48,7 +32,7 @@ public class TickerService {
 
     @Transactional
     public void deleteAllDataFor(String ticker) {
-        for (String table : DB_TABLES) {
+        for (String table : Constants.DB_TABLES) {
             String column = "daily_prices_json".equals(table) ? "symbol" : "ticker";
             String sql = STR."DELETE FROM \{table} WHERE \{column} = ?1";
 
--- src/main/java/stock/price/analytics/util/Constants.java
+++ src/main/java/stock/price/analytics/util/Constants.java
@@ -26,4 +26,18 @@ public final class Constants {
     public static final Double INTRADAY_SPIKE_PERCENTAGE = 0.02d; // percentage of price movement between imports (20m) used for alerting spikes
     public static final String HTTP_LOCALHOST = "http://localhost:";
     public static final String YAHOO_QUOTES_IMPORT_ENDPOINT = "/yahoo-quotes/import";
+    public static final List<String> DB_TABLES = List.of(
+            "daily_prices_json",
+            "daily_prices",
+            "weekly_prices",
+            "monthly_prices",
+            "quarterly_prices",
+            "yearly_prices",
+            "stocks",
+            "price_gaps",
+            "fvg",
+            "high_low4w",
+            "high_low52w",
+            "highest_lowest"
+    );
 }
