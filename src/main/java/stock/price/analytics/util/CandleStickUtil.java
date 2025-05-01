495c468b749975560cd70305ae111d024c489413
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-05-01 11:50:52 +0300

    return false instead of throwing exception, refactor name

--- src/main/java/stock/price/analytics/util/CandleStickUtil.java
+++ src/main/java/stock/price/analytics/util/CandleStickUtil.java
@@ -4,11 +4,11 @@ import stock.price.analytics.model.prices.ohlc.AbstractPrice;
 
 public final class CandleStickUtil {
 
-    public static boolean isTightCandleStick(AbstractPrice price, Double avg15DaysRange) {
-        if (avg15DaysRange == null || avg15DaysRange <= 0) {
-            throw new IllegalArgumentException("Average range must be a positive number");
+    public static boolean isTightCandleStick(AbstractPrice price, Double avgRange) {
+        if (avgRange == null || avgRange <= 0) {
+            return false;
         }
         double range = price.getHigh() - price.getLow();
-        return range <= avg15DaysRange * 0.5d;
+        return range <= avgRange * 0.5d;
     }
 }
