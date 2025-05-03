cd02b1dde10e51cab318144c29d44e970cee563f
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-05-03 18:59:38 +0300

    refactor variable names

--- src/main/java/stock/price/analytics/util/TradingDateUtil.java
+++ src/main/java/stock/price/analytics/util/TradingDateUtil.java
@@ -60,30 +60,30 @@ public final class TradingDateUtil {
                 .orElseThrow();
     }
 
-    public static boolean isWithinSameTimeframe(LocalDate date, LocalDate latestEndDateWMY, StockTimeframe timeframe) {
+    public static boolean isWithinSameTimeframe(LocalDate first, LocalDate second, StockTimeframe timeframe) {
         return switch (timeframe) {
-            case DAILY -> date.isEqual(latestEndDateWMY);
-            case WEEKLY -> sameWeek(date, latestEndDateWMY);
-            case MONTHLY -> sameMonth(date, latestEndDateWMY);
-            case QUARTERLY -> sameQuarter(date, latestEndDateWMY);
-            case YEARLY -> sameYear(date, latestEndDateWMY);
+            case DAILY -> first.isEqual(second);
+            case WEEKLY -> sameWeek(first, second);
+            case MONTHLY -> sameMonth(first, second);
+            case QUARTERLY -> sameQuarter(first, second);
+            case YEARLY -> sameYear(first, second);
         };
     }
 
-    private static boolean sameWeek(LocalDate firstDate, LocalDate secondDate) {
-        return firstDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)).isEqual(secondDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)));
+    private static boolean sameWeek(LocalDate first, LocalDate second) {
+        return first.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)).isEqual(second.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY)));
     }
 
-    private static boolean sameMonth(LocalDate firstDate, LocalDate secondDate) {
-        return firstDate.getYear() == secondDate.getYear() && firstDate.getMonthValue() == secondDate.getMonthValue();
+    private static boolean sameMonth(LocalDate first, LocalDate second) {
+        return first.getYear() == second.getYear() && first.getMonthValue() == second.getMonthValue();
     }
 
-    private static boolean sameQuarter(LocalDate firstDate, LocalDate secondDate) {
-        return firstDate.getYear() == secondDate.getYear() &&
-               firstDate.getMonth().firstMonthOfQuarter() == secondDate.getMonth().firstMonthOfQuarter();
+    private static boolean sameQuarter(LocalDate first, LocalDate second) {
+        return first.getYear() == second.getYear() &&
+               first.getMonth().firstMonthOfQuarter() == second.getMonth().firstMonthOfQuarter();
     }
 
-    private static boolean sameYear(LocalDate firstDate, LocalDate secondDate) {
-        return firstDate.getYear() == secondDate.getYear();
+    private static boolean sameYear(LocalDate first, LocalDate second) {
+        return first.getYear() == second.getYear();
     }
 }
