4ebec59f22c12815f03e7841a6cae90385c9ef6e
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-05-10 12:53:00 +0300

    general query to update stocks opening prices for all timeframes

--- src/main/java/stock/price/analytics/util/QueryUtil.java
+++ src/main/java/stock/price/analytics/util/QueryUtil.java
@@ -298,4 +298,21 @@ public final class QueryUtil {
                 GROUP BY ticker
                 """;
     }
+
+    public static String updateStocksWithOpeningPriceDiscrepancyFor(StockTimeframe timeframe) {
+        String prefix = timeframe.stockPrefix();
+        String dbTable = timeframe.dbTableOHLC();
+        String intervalPeriod = timeframe.toIntervalPeriod();
+        return STR."""
+                WITH discrepancies AS (
+                SELECT s.ticker, p.open
+                FROM public.stocks s
+                JOIN \{dbTable} p ON p.date = DATE_TRUNC('\{intervalPeriod}', s.last_updated) AND p.ticker = s.ticker
+                WHERE s.delisted_date IS NULL AND (p.open <> s.\{prefix}open)
+                )
+                UPDATE stocks s SET \{prefix}open = dscr.open
+                FROM discrepancies dscr
+                WHERE s.ticker = dscr.ticker;
+                """;
+    }
 }
