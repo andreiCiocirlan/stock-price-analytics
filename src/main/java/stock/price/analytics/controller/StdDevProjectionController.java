3ff2b72d8721c5722fc1ed0a0afeb0004828c1aa
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-07-05 12:15:05 +0300

    optimize imports

--- src/main/java/stock/price/analytics/controller/StdDevProjectionController.java
+++ src/main/java/stock/price/analytics/controller/StdDevProjectionController.java
@@ -1,7 +1,10 @@
 package stock.price.analytics.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import stock.price.analytics.model.dto.StdDevProjectionDTO;
 import stock.price.analytics.service.StdDevProjectionService;
 
