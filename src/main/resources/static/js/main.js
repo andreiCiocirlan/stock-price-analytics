a306e9aac65cb022daebd1520ebcef324d1b8e5c
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-07-08 10:42:45 +0300

    remove onchange, done in main.js with event listener

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,12 +1,19 @@
-//import { handleTimeFrameButtonClick, dispatchTimeFrameChangeEvent } from './stock-performance.js';
-
 import { getChartConfig } from './chartConfig.js';
-import { handleTimeFrameButtonClick } from './stock-performance.js';
+import { handleTimeFrameButtonClick, dispatchTimeFrameChangeEvent } from './stock-performance.js';
 import { addProjectionBandsSVG } from './projection-utils.js';
 import { updateOHLCChart } from './stock-graph.js';
 
 // Wait for DOM to be fully loaded
 document.addEventListener('DOMContentLoaded', () => {
+    const dropdown = document.getElementById('positivePerfFirst');
+    if (dropdown) {
+        dropdown.addEventListener('change', dispatchTimeFrameChangeEvent);
+    }
+
+    document.querySelectorAll('select.form-control').forEach(el => {
+      el.addEventListener('change', dispatchTimeFrameChangeEvent);
+    });
+
   document.getElementById('btn-daily').addEventListener('click', () => {
     handleTimeFrameButtonClick('DAILY');
   });
