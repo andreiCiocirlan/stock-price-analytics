f7a59541f351d41c5127030b72c065900fd95d34
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-07-30 15:54:46 +0300

    save price gaps intraday schedule

--- src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
+++ src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
@@ -3,6 +3,8 @@ package stock.price.analytics.scheduler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import stock.price.analytics.cache.CacheService;
+import stock.price.analytics.model.prices.enums.StockTimeframe;
 import stock.price.analytics.service.FairValueGapService;
 import stock.price.analytics.service.PriceGapService;
 
@@ -12,11 +14,15 @@ public class IntradayScheduler {
 
     private final FairValueGapService fairValueGapService;
     private final PriceGapService priceGapService;
+    private final CacheService cacheService;
 
     // 10 15,35,55 9-16 * * MON-FRI
     @Scheduled(cron = "${cron.intraday.gaps.update}", zone = "${cron.timezone}")
     public void updateGapsIntraday() {
         fairValueGapService.saveNewFVGsAndUpdateHighLowAndClosedAllTimeframes();
+        for (StockTimeframe timeframe : StockTimeframe.values()) {
+            priceGapService.savePriceGapsTodayFor(cacheService.getCachedTickers(), timeframe);
+        }
         priceGapService.closePriceGaps();
     }
 
