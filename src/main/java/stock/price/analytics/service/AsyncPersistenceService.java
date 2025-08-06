46db0c9723ff81e46437deaa4a35401bf2f47645
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-08-06 11:42:37 +0300

    add consistent try catch

--- src/main/java/stock/price/analytics/service/AsyncPersistenceService.java
+++ src/main/java/stock/price/analytics/service/AsyncPersistenceService.java
@@ -1,5 +1,6 @@
 package stock.price.analytics.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -7,24 +8,37 @@ import stock.price.analytics.model.BusinessEntity;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class AsyncPersistenceService extends SyncPersistenceService {
 
     @Async
     @Override
     public void partitionDataAndSaveNoLogging(List<? extends BusinessEntity> entities, JpaRepository<? extends BusinessEntity, Long> repository) {
-        super.partitionDataAndSaveNoLogging(entities, repository);
+        try {
+            super.partitionDataAndSaveNoLogging(entities, repository);
+        } catch (Exception e) {
+            log.error("Error in async save", e);
+        }
     }
 
     @Async
     @Override
     public void partitionDataAndSaveWithLogTime(List<? extends BusinessEntity> entities, JpaRepository<? extends BusinessEntity, Long> repository, String functionName) {
-        super.partitionDataAndSaveWithLogTime(entities, repository, functionName);
+        try {
+            super.partitionDataAndSaveWithLogTime(entities, repository, functionName);
+        } catch (Exception e) {
+            log.error("Error in async save", e);
+        }
     }
 
     @Async
     @Override
     public void partitionDataAndSave(List<? extends BusinessEntity> entities, JpaRepository<? extends BusinessEntity, Long> repository) {
-        super.partitionDataAndSave(entities, repository);
+        try {
+            super.partitionDataAndSave(entities, repository);
+        } catch (Exception e) {
+            log.error("Error in async save", e);
+        }
     }
 }
