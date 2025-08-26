6304791aa51d0955ef4c976d2e25949e56f16c1e
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-08-26 11:48:23 +0300

    refactor high low 5-10% logic

--- src/main/java/stock/price/analytics/model/prices/enums/PricePerformanceMilestone.java
+++ src/main/java/stock/price/analytics/model/prices/enums/PricePerformanceMilestone.java
@@ -45,15 +45,14 @@ public enum PricePerformanceMilestone implements StockPriceMilestone<StockHighLo
 
     @Override
     public boolean isMetFor(StockHighLowForPeriodContext context) {
+        double latestPrice = context.stock().getClose();
+        double low = context.highLowForPeriod().getLow();
+        double high = context.highLowForPeriod().getHigh();
         return switch (this) {
-            case HIGH_52W_95, HIGH_4W_95, HIGH_ALL_TIME_95 ->
-                    context.highLowForPeriod().getLow() != context.highLowForPeriod().getHigh() && (1 - (1 - (context.stock().getClose() - context.highLowForPeriod().getLow()) / (context.highLowForPeriod().getHigh() - context.highLowForPeriod().getLow()))) > 0.95;
-            case LOW_52W_95, LOW_4W_95, LOW_ALL_TIME_95 ->
-                    context.highLowForPeriod().getLow() != context.highLowForPeriod().getHigh() && (1 - (context.stock().getClose() - context.highLowForPeriod().getLow()) / (context.highLowForPeriod().getHigh() - context.highLowForPeriod().getLow())) > 0.95;
-            case HIGH_52W_90, HIGH_4W_90, HIGH_ALL_TIME_90 ->
-                    context.highLowForPeriod().getLow() != context.highLowForPeriod().getHigh() && (1 - (1 - (context.stock().getClose() - context.highLowForPeriod().getLow()) / (context.highLowForPeriod().getHigh() - context.highLowForPeriod().getLow()))) > 0.90;
-            case LOW_52W_90, LOW_4W_90, LOW_ALL_TIME_90 ->
-                    context.highLowForPeriod().getLow() != context.highLowForPeriod().getHigh() && (1 - (context.stock().getClose() - context.highLowForPeriod().getLow()) / (context.highLowForPeriod().getHigh() - context.highLowForPeriod().getLow())) > 0.90;
+            case HIGH_52W_95, HIGH_4W_95, HIGH_ALL_TIME_95 -> latestPrice >= high * 0.95 && latestPrice <= high;
+            case HIGH_52W_90, HIGH_4W_90, HIGH_ALL_TIME_90 -> latestPrice >= high * 0.90 && latestPrice <= high;
+            case LOW_52W_95, LOW_4W_95, LOW_ALL_TIME_95 -> latestPrice >= low && latestPrice <= low * 1.05;
+            case LOW_52W_90, LOW_4W_90, LOW_ALL_TIME_90 -> latestPrice >= low && latestPrice <= low * 1.10;
         };
     }
 }
