cc67fd7eaa1ccc83157b4cdbf2fcfd1cec8020c7
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-11-17 13:51:15 +0200

    add PRICE_HL_INSIDE_FVG

--- src/main/java/stock/price/analytics/model/prices/enums/TradingIdea.java
+++ src/main/java/stock/price/analytics/model/prices/enums/TradingIdea.java
@@ -2,6 +2,6 @@ package stock.price.analytics.model.prices.enums;
 
 public enum TradingIdea {
 
-    NONE, COMPRESSED_PRICE, PRICE_INSIDE_FVG
+    NONE, COMPRESSED_PRICE, PRICE_INSIDE_FVG, PRICE_HL_INSIDE_FVG
 
 }
