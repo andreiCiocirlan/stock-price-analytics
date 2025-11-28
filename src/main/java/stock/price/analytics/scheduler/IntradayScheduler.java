19bf8feea9a713284adbb80ce8309a7067b647c3
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-11-28 15:13:55 +0200

    save price gaps if import not done for timeframe (saves once per day, week etc.)

--- src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
+++ src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
@@ -3,14 +3,17 @@ package stock.price.analytics.scheduler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import stock.price.analytics.model.prices.enums.StockTimeframe;
 import stock.price.analytics.service.FairValueGapService;
 import stock.price.analytics.service.PriceGapService;
+import stock.price.analytics.service.PriceService;
 
 @Component
 @RequiredArgsConstructor
 public class IntradayScheduler {
 
     private final FairValueGapService fairValueGapService;
+    private final PriceService priceService;
     private final PriceGapService priceGapService;
 
     // 10 15,35,55 9-16 * * MON-FRI
@@ -18,6 +21,11 @@ public class IntradayScheduler {
     public void updateGapsIntraday() {
         fairValueGapService.saveNewFVGsAndUpdateHighLowAndClosedAllTimeframes();
         fairValueGapService.closeFVGsForAllTimeframes();
+        for (StockTimeframe timeframe : StockTimeframe.values()) {
+            if (!priceService.isFirstImportDoneFor(timeframe)) {
+                priceGapService.savePriceGapsTodayFor(timeframe);
+            }
+        }
         priceGapService.closePriceGaps();
     }
 
