1c846f1062cf9fc2b30a74adb87c076d5c172cb2
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2025-12-04 14:23:17 +0200

    price gap creation change timing

--- src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
+++ src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
@@ -24,7 +24,7 @@ public class IntradayScheduler {
         priceGapService.closePriceGaps();
     }
 
-    // 10 35,55 9 * * MON-FRI
+    // 20 35,55 9 * * MON-FRI
     @Scheduled(cron = "${cron.intraday.price.gaps.create}", zone = "${cron.timezone}")
     public void createPriceGapsIntraday() {
         for (StockTimeframe timeframe : StockTimeframe.values()) {
