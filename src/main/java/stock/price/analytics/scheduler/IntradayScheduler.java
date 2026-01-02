e92e2d0ca9c9139fd6f202e7616a1361846b6961
Andrei Ciocîrlan <andreiciocirlan93@gmail.com>
2026-01-02 12:43:12 +0200

    remove unused priceService

--- src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
+++ src/main/java/stock/price/analytics/scheduler/IntradayScheduler.java
@@ -6,14 +6,12 @@ import org.springframework.stereotype.Component;
 import stock.price.analytics.model.prices.enums.StockTimeframe;
 import stock.price.analytics.service.FairValueGapService;
 import stock.price.analytics.service.PriceGapService;
-import stock.price.analytics.service.PriceService;
 
 @Component
 @RequiredArgsConstructor
 public class IntradayScheduler {
 
     private final FairValueGapService fairValueGapService;
-    private final PriceService priceService;
     private final PriceGapService priceGapService;
 
     // 10 15,35,55 9-16 * * MON-FRI
